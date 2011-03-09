d3fd18a7002c15336c25e2ec0ddfb4cd7e104183
Alexey Nayden <alexey.nayden@gmail.com>
2011-03-09 19:16:48 +0300

    Device users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rosa::Application.routes.draw do
+  devise_for :users
+
   root :to => "platforms#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+require 'digest/md5'
+
+TEST_USERS = 
+  [
+    ['yaroslav@markin.net'        , 'Yaroslav Markin']  ,
+    ['timothy.tsvetkov@gmail.com' , 'Timothy Tsvetkov'] ,
+    ['alexey.nayden@gmail.com'    , 'Alexey Nayden']
+  ]
+
+TEST_USERS.each do |tuser|
+  email = tuser[0]
+  next if User.find_by_email(email)
+  name = tuser[1]
+  pass = Digest::MD5.hexdigest(name)[0..6]
+  User.create! :name => name, :email => email, :password => pass, :password_confirmation => pass
+  puts "Created user #{name} (#{email}) and password #{pass}"
+end
