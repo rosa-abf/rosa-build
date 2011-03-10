eccb4e0a6c6a1bc531e3e8498a57a9e5e5ceaa01
Alexey Nayden <alexey.nayden@gmail.com>
2011-03-10 16:38:50 +0300

    Project/platform management

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,22 +1,22 @@
 class Project < ActiveRecord::Base
   belongs_to :platform
 
-  validate :name, :uniqueness => true, :presence => true
-  validate :unixname, :uniqueness => true, :presence => true
-  validate :validate_unixname
+  validate :name, :uniqueness => true, :presence => true, :allow_nil => false, :allow_blank => false
+  validate :unixname, :uniqueness => true, :presence => true, :format => { :with => /^[a-zA-Z0-9\-.]+$/ }, :allow_nil => false, :allow_blank => false
 
   before_validation :generate_unixname
 
   include Project::HasRepository
 
+  # Redefining a method from Project::HasRepository module to reflect current situation
+  def git_repo_path
+    @git_repo_path ||= File.join(APP_CONFIG['root_path'], platform.unixname, project.unixname, project.unixname + '.git')
+  end
+
   protected
 
     def generate_unixname
-      #TODO: Implement unixname generation 
+      self.unixname = name.gsub(/[^a-zA-Z0-9\-.]/, '-')
+      #TODO: Fix non-unique unixname
     end
-
-    def validate_unixname
-      #TODO: Implement unixname validation
-    end
-
 end
