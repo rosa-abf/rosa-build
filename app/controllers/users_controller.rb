0ff80b3e16e9807b5c4764b4798ddc1c4d4e70c8
Alexey Nayden <alexey.nayden@gmail.com>
2011-03-11 00:48:15 +0300

    User management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,40 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def destroy
+    User.destroy params[:id]
+    redirect_to users_path
+  end
+
+  def create
+    @user = User.new params[:user]
+    if @user.save
+      flash[:notice] = t('flash.user.saved')
+      redirect_to users_path
+    else
+      flash[:error] = t('flash.user.save_error')
+      render :action => :new
+    end
+  end
+
+  def update
+    @user = User.find params[:id]
+    if @user.update_attributes(params[:user])
+      flash[:notice] = t('flash.user.saved')
+      redirect_to users_path
+    else
+      flash[:error] = t('flash.user.save_error')
+      render :action => :edit
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
+require 'digest/md5'
 class User < ActiveRecord::Base
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+
+  before_validation :generate_password, :on => :create
+  after_create :send_notification_email
+
+  protected
+
+    def generate_password
+      self.password = self.password_confirmation = Digest::MD5.hexdigest(Date.today.to_s)[0..6]
+    end
+
+    def send_notification_email
+      UserMailer.new_user_notification(self).deliver
+    end
 end
