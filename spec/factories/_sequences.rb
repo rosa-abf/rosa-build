a655d0eb6b7e8e75eb0ed582fcb64ef8e8cf3233
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2011-03-17 17:03:02 +0300

    sequences for the factory girl

---
+++ spec/factories/_sequences.rb
@@ -0,0 +1,21 @@
+Factory.sequence :integer do |n|
+  n
+end
+
+Factory.sequence :string do |n|
+  "Lorem ipsum #{n}"
+end
+
+Factory.sequence :email do |n|
+  "email#{n}@example.com"
+end
+
+Factory.sequence :text do |n|
+  "#{n}. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+end
+
+Factory.sequence :text_file do |n|
+  stringio = StringIO.new("this is file content #{n}")
+  stringio.instance_eval("def original_filename; 'stringio#{n}.txt'; end ")
+  stringio
+end
