3524d03703790c760936e86b847638a96efe0c98
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2011-03-29 21:27:56 +0400

    xxx

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -48,9 +48,9 @@ before_fork do |server, worker|
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
+#      someone else did our job for us
     end
-  end
+#  end
 end
 
 
