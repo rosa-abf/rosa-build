e984500a69e30830ff0ecc655c9e865a6c698dcd
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2011-03-31 03:52:34 +0400

    custom form errors renderer

---
+++ config/initializers/render_errors_in_forms.rb
@@ -0,0 +1,7 @@
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  if html_tag =~ /<label/
+    %|<div class="fieldWithErrors">#{html_tag} <span class="error">#{[instance.error_message].join(', ')}</span></div>|.html_safe
+  else
+    html_tag
+  end
+end
