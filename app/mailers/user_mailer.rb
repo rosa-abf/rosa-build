327452e69b83714fb63909bb4fc23ca2d2504b83
Alexey Nayden <alexey.nayden@gmail.com>
2011-03-31 16:21:43 +0400

    html email notification

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default :from => APP_CONFIG['no-reply-email'], :content_type => "text/plain"
+  default :from => APP_CONFIG['no-reply-email']
 
   def new_user_notification(user)
     @user = user
     mail(:to => user.email, :subject => "Регистрация на проекте «#{APP_CONFIG['project_name']}»") do |format|
-      format.text
+      format.html
     end
   end
 end
