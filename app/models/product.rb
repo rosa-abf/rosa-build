56b17dd430d5898eca0a14e5d0791bab7003a064
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2011-04-11 21:05:40 +0400

    useless commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,11 @@
 class Product < ActiveRecord::Base
+  NEVER_BUILT = 2
+  BUILD_COMPLETED = 0
+  BUILD_FAILED = 1
+
   validates :name, :presence => true, :uniqueness => true
   validates :platform_id, :presence => true
   validates :build_status, :inclusion => { :in => [ NEVER_BUILT, BUILD_COMPLETED, BUILD_FAILED ] }
 
   belongs_to :platform
-
-  NEVER_BUILT = 2
-  BUILD_COMPLETED = 0
-  BUILD_FAILED = 1
 end
