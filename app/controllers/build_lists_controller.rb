cbab2a76717cc23bd5b2915b1a9288781de2bb29
Alexey Nayden <alexey.nayden@gmail.com>
2011-04-11 12:34:34 +0400

    minor fix

--- app/controllers/build_lists_controller.rb
+++ app/controllers/build_lists_controller.rb
@@ -1,5 +1,5 @@
 class BuildListsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:status_build, :pre_build, :post_build]
   before_filter :find_platform, :only => [:index, :filter]
   before_filter :find_repository, :only => [:index, :filter]
   before_filter :find_project, :only => [:index, :filter]
