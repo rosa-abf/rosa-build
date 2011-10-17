ff5aa09c30124891f41e3553dfcf3fd0997cdd01
George Vinogradov <g.vinogradov@itima.ru>
2011-10-17 18:16:21 +0400

    Added method to determine current acter (group or user)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,12 @@ class ApplicationController < ActionController::Base
       end
     end
 
+    def get_acter
+      return User.find params[:user_id] if params[:user_id]
+      return Group.find params[:group_id] if params[:group_id]
+      return current_user
+    end
+
     def authenticate_build_service!
       if request.remote_ip != APP_CONFIG['build_service_ip']
         render :nothing => true, :status => 403
