282e97cbba84874128bf33f15cb4b671cdb88d5a
Pavel Chipiga <pavel.chipiga@gmail.com>
2011-10-28 01:55:49 +0300

    Git increase gimeout. Refs #2136

--- lib/ext/active_support/object.rb
+++ lib/ext/active_support/object.rb
@@ -2,7 +2,7 @@ require 'gitolito'
 
 class Object
   def with_ga(&block)
-    ::Gitolito::GitoliteAdmin.thread_safe(File.join(APP_CONFIG['root_path'], 'gitolite-admin'), {:wait_lock => true, :seconds => 5}) do |ga|
+    ::Gitolito::GitoliteAdmin.thread_safe(File.join(APP_CONFIG['root_path'], 'gitolite-admin'), {:wait_lock => true, :seconds => 15}) do |ga|
       block.call(ga)
     end
     # ga = Gitolito::GitoliteAdmin.new File.join(APP_CONFIG['root_path'], 'gitolite-admin'); block.call(ga)
