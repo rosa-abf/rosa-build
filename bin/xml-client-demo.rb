f39e51e516356ee6550b7acccbf59203769b50c9
Pavel Chipiga <pavel.chipiga@gmail.com>
2011-10-30 21:01:08 +0200

    Fix XML RPC responses. Refs #2162

--- bin/xml-client-demo.rb
+++ bin/xml-client-demo.rb
@@ -2,7 +2,7 @@
 
 require 'rubygems'
 require 'xmlrpc/client'
-require 'awesome_print'
+require 'pp'
 
 # Please correctly fill following vars
 @host = 'localhost'
@@ -12,26 +12,26 @@ require 'awesome_print'
 
 puts 'PLATFORMS'
 client = XMLRPC::Client.new(@host, '/api/xmlrpc', @port, nil, nil, @user, @password, false, 900)
-ap client.call("platforms")
+pp client.call("platforms")
 
 puts 'USER PROJECTS'
 client = XMLRPC::Client.new(@host, '/api/xmlrpc', @port, nil, nil, @user, @password, false, 900)
-ap client.call("user_projects")
+pp client.call("user_projects")
 
 puts 'PROJECT VERSIONS'
 client = XMLRPC::Client.new(@host, '/api/xmlrpc', @port, nil, nil, @user, @password, false, 900)
 project_id = 1    # FIXME!
-ap client.call("project_versions", project_id)
+pp client.call("project_versions", project_id)
 
 puts 'BUILD STATUS'
 client = XMLRPC::Client.new(@host, '/api/xmlrpc', @port, nil, nil, @user, @password, false, 900)
 build_list_id = 1 # FIXME
-ap client.call("build_status", build_list_id)
+pp client.call("build_status", build_list_id)
 
 puts 'BUILD PACKET'
 client = XMLRPC::Client.new(@host, '/api/xmlrpc', @port, nil, nil, @user, @password, false, 900)
 project_id = 1    # FIXME
 repo_id = 1       # FIXME
-ap client.call("build_packet", project_id, repo_id)
+pp client.call("build_packet", project_id, repo_id)
 
 puts 'DONE'
