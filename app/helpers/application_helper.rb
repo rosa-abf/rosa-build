92011e493bf417f0643904ad1412f9377a477f79
konstantin.grabar <santaux@gmail.com>
2011-11-02 00:18:22 +0400

    [refs #2161] Add new titles to application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def choose_title
+    title = if ['categories', 'personal_repositories', 'downloads'].include?(controller.controller_name)
+      APP_CONFIG['repo_project_name']
+    else
+      APP_CONFIG['project_name']
+    end
+    
+    return title
+  end
 end
