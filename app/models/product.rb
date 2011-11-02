948bd0f92d375653eed65065b358e0abdd2e1546
Vladimir Sharshov <vsharshov@gmail.com>
2011-11-02 23:20:47 +0400

    Add tar.bz2 support

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,7 +12,7 @@ class Product < ActiveRecord::Base
   belongs_to :platform
 
   has_attached_file :tar
-  validates_attachment_content_type :tar, :content_type => ["application/gnutar", "application/x-compressed", "application/x-gzip", "application/x-bzip2"], :message => I18n.t('layout.products.invalid_content_type')
+  validates_attachment_content_type :tar, :content_type => ["application/gnutar", "application/x-compressed", "application/x-gzip", "application/x-bzip2", "application/x-tar"], :message => I18n.t('layout.products.invalid_content_type')
 
   after_validation :merge_tar_errors
 
