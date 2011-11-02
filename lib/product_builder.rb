ae6919a24007ebd339afc1053bca5bf5b2012e62
Vladimir Sharshov <vsharshov@gmail.com>
2011-11-03 00:25:25 +0400

    Add url for downloading file archive

--- lib/product_builder.rb
+++ lib/product_builder.rb
@@ -9,7 +9,7 @@ class ProductBuilder
     @@client ||= XMLRPC::Client.new3(:host => APP_CONFIG['product_builder_ip'], :port => APP_CONFIG['product_builder_port'], :path => APP_CONFIG['product_builder_path'])
   end
 
-  def self.create_product product_id, path, kstemplate, menuxml, build, counter, packages
-    self.client.call('create_product', product_id.to_s, path, kstemplate, menuxml, build, counter, packages)
+  def self.create_product product_id, path, kstemplate, menuxml, build, counter, packages, tar_url
+    self.client.call('create_product', product_id.to_s, path, kstemplate, menuxml, build, counter, packages, tar_url)
   end
 end
