d9a03013b49a500960db0d2b3968db7cc401f8d7
konstantin.grabar <santaux@gmail.com>
2011-11-03 15:01:29 +0400

    [refs #2161] Fixes projects filter

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,12 @@ class ProjectsController < ApplicationController
   before_filter :check_global_access, :only => [:index, :new, :create]#:except => :auto_build
 
   def index
-    @projects = Project.visible_to(current_user).where(:name => params[:query]).paginate(:page => params[:project_page])
+    if params[:query]
+      @projects = Project.visible_to(current_user).where(:name => params[:query]).paginate(:page => params[:project_page])
+    else
+      @projects = Project.visible_to(current_user).paginate(:page => params[:project_page])
+    end
+    
     @own_projects = current_user.own_projects
     @part_projects = current_user.projects - @own_projects
   end
