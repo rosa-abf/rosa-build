da784b0bc293ebc31567a4cdad8c59edeb67b026
Pavel Chipiga <pavel.chipiga@gmail.com>
2011-11-10 11:04:24 +0200

    Improve git_repo_url helper. Refs #2162

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,5 +1,10 @@
 module ProjectsHelper
   def git_repo_url(name)
-    "ssh://git@#{request.host}:1822/#{name}.git"
+    port = APP_CONFIG['ssh_port'] || 22
+    if port == 22
+      "git@#{request.host}:#{name}.git"
+    else
+      "ssh://git@#{request.host}:#{port}/#{name}.git"
+    end
   end
 end
