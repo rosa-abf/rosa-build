a1afb81bb6e88c77f4fed764330efab80077b1eb
Pavel Chipiga <pavel.chipiga@gmail.com>
2011-11-21 23:55:52 +0200

    Fix platform mount scripts. Refs #2261

--- bin/mount_rsync.sh
+++ bin/mount_rsync.sh
@@ -1,7 +1,7 @@
 for f in `ls /srv/rosa_build/current/tmp/mount`
 do
-  mkdir "/srv/rosa_build/shared/downloads/$f"
+  mkdir -p "/srv/rosa_build/shared/downloads/$f"
   mount --bind "/home/share/platforms/$f" "/srv/rosa_build/shared/downloads/$f"
-  mv -f "/srv/rosa_build/current/tmp/mount/$f/*" "/home/share/platforms/$f"
+  cp -f "/srv/rosa_build/current/tmp/mount/$f/*" "/home/share/platforms/$f"
   rm -Rf "/srv/rosa_build/current/tmp/mount/$f"
 done
