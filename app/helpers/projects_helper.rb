a9026bd6f8a48cc6e303162f6ffc6d6487352387
Pavel Chipiga <pavel.chipiga@gmail.com>
2011-11-22 20:58:23 +0200

    Change git_repo_url logic. Refs #2261

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,6 +1,6 @@
 module ProjectsHelper
-  def git_repo_url(name, read_only = true)
-    if current_user and !read_only
+  def git_repo_url(name)
+    if current_user
       "http://#{current_user.uname}@#{request.host_with_port}/#{name}.git"
     else
       "http://#{request.host_with_port}/#{name}.git"
