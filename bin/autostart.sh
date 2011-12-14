31bd8d799fd8bc5dff3c48cf8599f397afb4bc8f
Pavel Chipiga <pavel.chipiga@gmail.com>
2011-12-14 15:04:16 +0200

    Fix ENV var syntax. Fix autostart. Refs #1

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -2,7 +2,7 @@
 
 for f in `ls /srv`
 do
-  sudo -u rosa bundle exec bluepill --no-privileged load /srv/$f/current/config/production.pill APP_NAME=$f
+  sudo -u rosa bundle exec APP_NAME=$f bluepill --no-privileged load /srv/$f/current/config/production.pill
 done
 
 /srv/rosa_build/current/bin/mount_downloads.sh
--- lib/recipes/bluepill.rb
+++ lib/recipes/bluepill.rb
@@ -1,21 +1,21 @@
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :bluepill do
-    set :bluepill_binary, "bundle exec bluepill --no-privileged"
+    set :bluepill_binary, "bundle exec APP_NAME=#{fetch :application} bluepill --no-privileged"
 
     desc "Stop processes that bluepill is monitoring and quit bluepill"
     task :quit, :roles => [:app], :on_error => :continue do
-      run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} stop APP_NAME=#{fetch :application}"
-      run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} quit APP_NAME=#{fetch :application}"
+      run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} stop"
+      run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} quit"
     end
 
     desc "Load bluepill configuration and start it"
     task :start, :roles => [:app] do
-      run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} load config/production.pill APP_NAME=#{fetch :application}"
+      run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} load config/production.pill"
     end
 
     desc "Prints bluepills monitored processes statuses"
     task :status, :roles => [:app] do
-      run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} status APP_NAME=#{fetch :application}"
+      run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} status"
     end
   end
 end
