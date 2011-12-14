37c8a50bf8b47709b2af62f4f9a03df748f336d4
Pavel Chipiga <pavel.chipiga@gmail.com>
2011-12-14 23:06:31 +0200

    Refactor and fix bluepill capistrano recipe. Refs #1

--- lib/recipes/bluepill.rb
+++ lib/recipes/bluepill.rb
@@ -1,31 +1,30 @@
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :bluepill do
-    set(:bluepill_binary) {"APP_NAME=#{fetch :application} bundle exec bluepill --no-privileged"}
+    set(:bluepill_binary) {"bundle exec bluepill --no-privileged"}
 
     desc "Load bluepill configuration and start it"
     task :start, :roles => [:app] do
-      run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} load config/production.pill"
+      run "cd #{fetch :current_path} && #{try_sudo} APP_NAME=#{fetch :application} #{bluepill_binary} load config/production.pill"
     end
 
     desc "Stop processes that bluepill is monitoring"
     task :stop, :roles => [:app], :on_error => :continue do
-      run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} stop"
+      run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} #{fetch :application} stop"
     end
 
     task :restart, :roles => [:app] do
-      # run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} restart #{fetch :application}"
-      stop; start
+      # run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} #{fetch :application} restart"
+      stop; quit; start
     end
 
     desc "Stop processes that bluepill is monitoring and quit bluepill"
     task :quit, :roles => [:app] do
-      # stop
-      run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} quit"
+      run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} #{fetch :application} quit"
     end
 
     desc "Prints bluepills monitored processes statuses"
     task :status, :roles => [:app] do
-      run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} status"
+      run "cd #{fetch :current_path} && #{try_sudo} #{bluepill_binary} #{fetch :application} status"
     end
   end
 end
