3b300f2ca4c36df39023ee87cd1e53ad6b265400
George Vinogradov <g.vinogradov@itima.ru>
2011-12-23 21:17:02 +0400

    [issue #79] Removed comments

--- spec/models/project_to_repository_spec.rb
+++ spec/models/project_to_repository_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe ProjectToRepository do
- # pending "add some examples to (or delete) #{__FILE__}"
   before(:each) do
     stub_rsync_methods
     @platform = Factory(:platform)
@@ -13,8 +12,6 @@ describe ProjectToRepository do
   end
 
   it 'should not add the same project in different repositories of same platform' do
-#    puts Platform.scoped.select('projects.*').joins(:repositories => :projects).where(:id => @platform.id)inspect
-    
     p2r = @second_repo.project_to_repositories.build :project_id => @project.id
     p2r.should_not be_valid
   end
