7d8d77a38633c7a209b67442c985d8d270a9a691
George Vinogradov <g.vinogradov@itima.ru>
2011-12-23 21:06:47 +0400

    [issue #79] Added validation to ProjectToRepository

--- spec/models/project_to_repository_spec.rb
+++ spec/models/project_to_repository_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe ProjectToRepository do
-  pending "add some examples to (or delete) #{__FILE__}"
+ # pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    stub_rsync_methods
+    @platform = Factory(:platform)
+    @first_repo = Factory(:repository, :platform_id => @platform.id)
+    @second_repo = Factory(:repository, :platform_id => @platform.id)
+    @project = Factory(:project)
+    @first_repo.projects << @project
+    @first_repo.save
+  end
+
+  it 'should not add the same project in different repositories of same platform' do
+#    puts Platform.scoped.select('projects.*').joins(:repositories => :projects).where(:id => @platform.id)inspect
+    
+    p2r = @second_repo.project_to_repositories.build :project_id => @project.id
+    p2r.should_not be_valid
+  end
 end
