a0aad7103e5c699e9f9f9fd864a5d0b2362b95e1
Alexander Machehin <alexander.machehin@gmail.com>
2012-01-17 17:17:27 +0600

    some fixes

--- app/models/project.rb
+++ app/models/project.rb
@@ -136,7 +136,7 @@ class Project < ActiveRecord::Base
 
   class << self
     def commit_comments(commit, project)
-     comments = Comment.where(:commentable_id => commit.id, :commentable_type => 'Grit::Commit')
+     comments = Comment.where(:commentable_id => commit.id, :commentable_type => 'Grit::Commit').order(:created_at)
      comments.each {|x| x.project = project}
     end
   end
