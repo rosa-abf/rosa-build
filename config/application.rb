3206258f7b764a35474cdaeafcb6965a4ae1d2cf
Vladimir Sharshov <vsharshov@gmail.com>
2012-01-20 00:06:43 +0400

    Change default locale to English

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Rosa
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :ru
+    config.i18n.default_locale = :en
 
     config.action_view.javascript_expansions[:defaults] = %w()
 
