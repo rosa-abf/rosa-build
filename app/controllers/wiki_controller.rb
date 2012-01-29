1ef2bc0f753c52f86bbd469322ff87bc11e79a5a
George Vinogradov <g.vinogradov@itima.ru>
2012-01-29 22:02:18 +0400

    [issue #64] Actions authorization moved to filters

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -11,266 +11,201 @@ class WikiController < ApplicationController
   before_filter :get_wiki
 
   def index
-    if can? :read, @project
-      @name = 'Home'
-      @page = @wiki.page(@name)
+    @name = 'Home'
+    @page = @wiki.page(@name)
 
-      show_or_create_page
-    else
-      redirect_to forbidden_path
-    end
+    show_or_create_page
   end
 
   def show
-    if can? :read, @project
-      @name = CGI.unescape(params['id'])
-      redirect_to project_wiki_index_path(@project) and return if @name == 'Home'
+    @name = CGI.unescape(params['id'])
+    redirect_to project_wiki_index_path(@project) and return if @name == 'Home'
 
-      ref = params['ref'] ? params['ref'] : @wiki.ref
-      @page = @wiki.page(@name, ref)
-      if !@page && @wiki.page(@name)
-        flash[:error] = t('flash.wiki.ref_not_exist')
-        redirect_to project_wiki_path(@project, CGI.escape(@name)) and return
-      end
-
-      show_or_create_page
-    else
-      redirect_to forbidden_path
+    ref = params['ref'] ? params['ref'] : @wiki.ref
+    @page = @wiki.page(@name, ref)
+    if !@page && @wiki.page(@name)
+      flash[:error] = t('flash.wiki.ref_not_exist')
+      redirect_to project_wiki_path(@project, CGI.escape(@name)) and return
     end
+
+    show_or_create_page
   end
 
   def edit
-    if can? :write, @project
-      @name = CGI.unescape(params[:id])
-      if page = @wiki.page(@name)
-        @page = page
-        @content = page.raw_data
-        render :edit
-      else
-        render :new
-      end
+    @name = CGI.unescape(params[:id])
+    if page = @wiki.page(@name)
+      @page = page
+      @content = page.raw_data
+      render :edit
     else
-      redirect_to forbidden_path
+      render :new
     end
   end
 
   def update
-    if can? :write, @project
-      @name = CGI.unescape(params[:id])
-      @page = @wiki.page(@name)
-      name = params[:rename] || @name
-      committer = Gollum::Committer.new(@wiki, commit)
-      commit_arg = {:committer => committer}
+    @name = CGI.unescape(params[:id])
+    @page = @wiki.page(@name)
+    name = params[:rename] || @name
+    committer = Gollum::Committer.new(@wiki, commit)
+    commit_arg = {:committer => committer}
 
-      update_wiki_page(@wiki, @page, params[:content], commit_arg, name, params[:format])
-      update_wiki_page(@wiki, @page.footer, params[:footer], commit_arg) if params[:footer]
-      update_wiki_page(@wiki, @page.sidebar, params[:sidebar], commit_arg) if params[:sidebar]
+    update_wiki_page(@wiki, @page, params[:content], commit_arg, name, params[:format])
+    update_wiki_page(@wiki, @page.footer, params[:footer], commit_arg) if params[:footer]
+    update_wiki_page(@wiki, @page.sidebar, params[:sidebar], commit_arg) if params[:sidebar]
 
-      committer.commit
+    committer.commit
 
-      flash[:notice] = t('flash.wiki.successfully_updated', :name => @name)
-      redirect_to project_wiki_path(@project, CGI.escape(@name))
-    else
-      redirect_to forbidden_path
-    end
+    flash[:notice] = t('flash.wiki.successfully_updated', :name => @name)
+    redirect_to project_wiki_path(@project, CGI.escape(@name))
   end
 
   def new
-    if can? :write, @project
-      @name = ''
-    else
-      redirect_to forbidden_path
-    end
+    @name = ''
   end
 
   def create
-    if can? :write, @project
-      @name = params['page']
-      format = params['format'].intern
+    @name = params['page']
+    format = params['format'].intern
 
-      begin
-        @wiki.write_page(@name, format, params['content'], commit)
-        redirect_to project_wiki_path(@project, CGI.escape(@name))
-      rescue Gollum::DuplicatePageError => e
-        flash[:error] = t("flash.wiki.duplicate_page", :name => @name)
-        render :action => :new
-      end
-    else
-      redirect_to forbidden_path
+    begin
+      @wiki.write_page(@name, format, params['content'], commit)
+      redirect_to project_wiki_path(@project, CGI.escape(@name))
+    rescue Gollum::DuplicatePageError => e
+      flash[:error] = t("flash.wiki.duplicate_page", :name => @name)
+      render :action => :new
     end
   end
 
   def destroy
-    if can? :write, @project
-      @name = CGI.unescape(params[:id])
-      page = @wiki.page(@name)
-      if page
-        @wiki.delete_page(page, commit.merge(:message => 'Page removed'))
-        flash[:notice] = t("flash.wiki.page_successfully_removed")
-      else
-        flash[:notice] = t("flash.wiki.page_not_found", :name => params[:id])
-      end
-      redirect_to project_wiki_index_path(@project)
+    @name = CGI.unescape(params[:id])
+    page = @wiki.page(@name)
+    if page
+      @wiki.delete_page(page, commit.merge(:message => 'Page removed'))
+      flash[:notice] = t("flash.wiki.page_successfully_removed")
     else
-      redirect_to forbidden_path
+      flash[:notice] = t("flash.wiki.page_not_found", :name => params[:id])
     end
+    redirect_to project_wiki_index_path(@project)
   end
 
   def git
-    redirect_to forbidden_path if cannot? :read, @project
   end
 
   def compare
-    if can? :read, @project
-      @name = CGI.unescape(params[:id])
-      if request.post?
-        @versions = params[:versions] || []
-        if @versions.size < 2
-          redirect_to history_project_wiki_path(@project, CGI.escape(@name))
-        else
-          redirect_to compare_versions_project_wiki_path(@project, CGI.escape(@name),
-                                                         sprintf('%s...%s', @versions.last, @versions.first))
-        end
-      elsif request.get?
-        @versions = params[:versions].split(/\.{2,3}/)
-        if @versions.size < 2
-          redirect_to history_project_wiki_path(@project, CGI.escape(@name))
-          return
-        end
-        page = @wiki.page(@name)
-        @diffs = [@wiki.repo.diff(@versions.first, @versions.last, page.path).first]
-        render :compare
+    @name = CGI.unescape(params[:id])
+    if request.post?
+      @versions = params[:versions] || []
+      if @versions.size < 2
+        redirect_to history_project_wiki_path(@project, CGI.escape(@name))
       else
-        redirect_to project_wiki_path(@project, CGI.escape(@name))
+        redirect_to compare_versions_project_wiki_path(@project, CGI.escape(@name),
+                                                       sprintf('%s...%s', @versions.last, @versions.first))
       end
+    elsif request.get?
+      @versions = params[:versions].split(/\.{2,3}/)
+      if @versions.size < 2
+        redirect_to history_project_wiki_path(@project, CGI.escape(@name))
+        return
+      end
+      page = @wiki.page(@name)
+      @diffs = [@wiki.repo.diff(@versions.first, @versions.last, page.path).first]
+      render :compare
     else
-      redirect_to forbidden_path
+      redirect_to project_wiki_path(@project, CGI.escape(@name))
     end
   end
 
   def compare_wiki
-    if can? :read, @project
-      if request.post?
-        @versions = params[:versions] || []
-        if @versions.size < 2
-          redirect_to history_project_wiki_index_path(@project)
-        else
-          redirect_to compare_versions_project_wiki_index_path(@project,
-                                                         sprintf('%s...%s', @versions.last, @versions.first))
-        end
-      elsif request.get?
-        @versions = params[:versions].split(/\.{2,3}/)
-        if @versions.size < 2
-          redirect_to history_project_wiki_index_path(@project)
-          return
-        end
-        @diffs = @wiki.repo.diff(@versions.first, @versions.last)
-        render :compare
+    if request.post?
+      @versions = params[:versions] || []
+      if @versions.size < 2
+        redirect_to history_project_wiki_index_path(@project)
       else
-        redirect_to project_wiki_path(@project, CGI.escape(@name))
+        redirect_to compare_versions_project_wiki_index_path(@project,
+                                                       sprintf('%s...%s', @versions.last, @versions.first))
       end
+    elsif request.get?
+      @versions = params[:versions].split(/\.{2,3}/)
+      if @versions.size < 2
+        redirect_to history_project_wiki_index_path(@project)
+        return
+      end
+      @diffs = @wiki.repo.diff(@versions.first, @versions.last)
+      render :compare
     else
-      redirect_to forbidden_path
+      redirect_to project_wiki_path(@project, CGI.escape(@name))
     end
   end
 
   def revert
-    if can? :write, @project
-      @name = CGI.unescape(params[:id])
-      @page = @wiki.page(@name)
-      sha1  = params[:sha1]
-      sha2  = params[:sha2]
+    @name = CGI.unescape(params[:id])
+    @page = @wiki.page(@name)
+    sha1  = params[:sha1]
+    sha2  = params[:sha2]
 
-      if @wiki.revert_page(@page, sha1, sha2, commit)
-        flash[:notice] = t("flash.wiki.revert_success")
-        redirect_to project_wiki_path(@project, CGI.escape(@name))
-      else
-        # if revert wasn't successful then redirect back to comparsion.
-        # if second commit version is missed, then second version is 
-        # params[:sha1] and first version is parent of params[:sha1]
-        # (see Gollum::Wiki#revert_page)
-        sha2, sha1 = sha1, "#{sha1}^" if !sha2
-        @versions = [sha1, sha2]
-        diffs     = @wiki.repo.diff(@versions.first, @versions.last, @page.path)
-        @diffs    = [diffs.first]
-        flash[:error]  = t("flash.wiki.patch_does_not_apply")
-        render :compare
-      end
+    if @wiki.revert_page(@page, sha1, sha2, commit)
+      flash[:notice] = t("flash.wiki.revert_success")
+      redirect_to project_wiki_path(@project, CGI.escape(@name))
     else
-      redirect_to forbidden_path
+      # if revert wasn't successful then redirect back to comparsion.
+      # if second commit version is missed, then second version is 
+      # params[:sha1] and first version is parent of params[:sha1]
+      # (see Gollum::Wiki#revert_page)
+      sha2, sha1 = sha1, "#{sha1}^" if !sha2
+      @versions = [sha1, sha2]
+      diffs     = @wiki.repo.diff(@versions.first, @versions.last, @page.path)
+      @diffs    = [diffs.first]
+      flash[:error]  = t("flash.wiki.patch_does_not_apply")
+      render :compare
     end
   end
 
   def revert_wiki
-    if can? :write, @project
-      sha1 = params[:sha1]
-      sha2 = params[:sha2]
-      if @wiki.revert_commit(sha1, sha2, commit)
-        flash[:notice] = t("flash.wiki.revert_success")
-        redirect_to project_wiki_index_path(@project)
-      else
-        sha2, sha1 = sha1, "#{sha1}^" if !sha2
-        @versions = [sha1, sha2]
-        diffs     = @wiki.repo.diff(@versions.first, @versions.last)
-        @diffs    = [diffs.first]
-        flash[:error] = t("flash.wiki.patch_does_not_apply")
-        render :compare
-      end
+    sha1 = params[:sha1]
+    sha2 = params[:sha2]
+    if @wiki.revert_commit(sha1, sha2, commit)
+      flash[:notice] = t("flash.wiki.revert_success")
+      redirect_to project_wiki_index_path(@project)
     else
-      redirect_to forbidden_path
+      sha2, sha1 = sha1, "#{sha1}^" if !sha2
+      @versions = [sha1, sha2]
+      diffs     = @wiki.repo.diff(@versions.first, @versions.last)
+      @diffs    = [diffs.first]
+      flash[:error] = t("flash.wiki.patch_does_not_apply")
+      render :compare
     end
   end
 
   def preview
-    if can? :write, @project
-      @name = params['page']
-      @page = @wiki.preview_page(@name, params['content'], params['format'])
-      @content = @page.formatted_data
-      @editable = false
-      render :show
-    else
-      redirect_to forbidden_path
-    end
+    @name = params['page']
+    @page = @wiki.preview_page(@name, params['content'], params['format'])
+    @content = @page.formatted_data
+    @editable = false
+    render :show
   end
 
   def history
-    if can? :read, @project
-      @name = CGI.unescape(params[:id])
-      if @page = @wiki.page(@name)
-        @versions = @page.versions
-      else
-        redirect_to :back
-      end
+    @name = CGI.unescape(params[:id])
+    if @page = @wiki.page(@name)
+      @versions = @page.versions
     else
-      redirect_to forbidden_path
+      redirect_to :back
     end
   end
 
   def wiki_history
-    if can? :read, @project
-      @versions = @wiki.log
-      render :history
-    else
-      redirect_to forbidden_path
-    end
+    @versions = @wiki.log
+    render :history
   end
 
   def search
-    if can? :read, @project
-      @query = params[:q]
-      @results = @wiki.search @query
-    else
-      redirect_to forbidden_path
-    end
+    @query = params[:q]
+    @results = @wiki.search @query
   end
 
   def pages
-    if can? :read, @project
-      @results = @wiki.pages
-      @ref = @wiki.ref
-    else
-      redirect_to forbidden_path
-    end
+    @results = @wiki.pages
+    @ref = @wiki.ref
   end
 
   protected
@@ -330,11 +265,11 @@ class WikiController < ApplicationController
     end
 
     def authorize_read_actions
-      puts "authorize action #{action_name}"
+      redirect_to forbidden_path and return if cannot? :read, @project
     end
 
     def authorize_write_actions
-      puts "authorize action #{action_name}"
+      redirect_to forbidden_path and return if cannot? :write, @project
     end
 end
 
