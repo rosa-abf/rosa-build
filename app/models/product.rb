34b105675a71236c84e84768950df1e812f963e3
Pavel Chipiga <pavel.chipiga@gmail.com>
2012-01-31 23:29:25 +0200

    Add 1 more mime type to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,7 +10,7 @@ class Product < ActiveRecord::Base
 
   has_attached_file :tar
 
-  validates_attachment_content_type :tar, :content_type => ["application/gnutar", "application/x-compressed", "application/x-gzip", "application/x-bzip2", "application/x-tar"], :message => I18n.t('layout.invalid_content_type')
+  validates_attachment_content_type :tar, :content_type => ["application/gnutar", "application/x-compressed", "application/x-gzip", "application/x-bzip", "application/x-bzip2", "application/x-tar"], :message => I18n.t('layout.invalid_content_type')
   validates :name, :presence => true, :uniqueness => {:scope => :platform_id}
 
   scope :recent, order("name ASC")
