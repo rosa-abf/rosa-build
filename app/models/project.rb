fde67a2c22e3790e65f6e87f65d158336546f0d6
Alexander Machehin <alexander.machehin@gmail.com>
2012-02-06 15:31:22 +0600

    [refs #134] add post receive hook to repos

--- app/models/project.rb
+++ app/models/project.rb
@@ -162,6 +162,11 @@ class Project < ActiveRecord::Base
     owner == user
   end
 
+  def self.process_hook(owner_uname, repo, newrev, oldrev, ref, newrev_type, oldrev_type)
+    rec = GitHook.new(owner_uname, repo, newrev, oldrev, ref, newrev_type, oldrev_type)
+    #ActivityFeedObserver.instance.after_create rec # for example
+  end
+
   protected
 
   def build_path(dir)
@@ -178,6 +183,7 @@ class Project < ActiveRecord::Base
 
   def create_git_repo
     is_root? ? Grit::Repo.init_bare(path) : parent.git_repository.repo.delay.fork_bare(path)
+    write_hook.delay
   end
 
   def destroy_git_repo
@@ -205,4 +211,10 @@ class Project < ActiveRecord::Base
     FileUtils.rm_rf wiki_path
   end
 
+  def write_hook
+    hook_file = File.join(path, 'hooks', 'post-receive')
+    FileUtils.cp(File.join(::Rails.root.to_s, 'lib', 'post-receive-hook'), hook_file)
+    #File.chmod(0775, hook_file) # need?
+  rescue Exception # FIXME
+  end
 end
