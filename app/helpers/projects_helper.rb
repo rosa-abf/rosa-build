f7b496bb39d4df977b121814bb29d363c9604241
George Vinogradov <g.vinogradov@itima.ru>
2012-02-15 20:30:56 +0400

    Some fixes:
      *  Changed default visibility to personal platforms to 'open'
      *  Changed 'Erase' button to simple link in comments partial
      *  Probably added domain name to links in mail notifications
      *  Fixed protocol in repo links form 'http' to 'https'

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,9 +2,9 @@
 module ProjectsHelper
   def git_repo_url(name)
     if current_user
-      "http://#{current_user.uname}@#{request.host_with_port}/#{name}.git"
+      "https://#{current_user.uname}@#{request.host_with_port}/#{name}.git"
     else
-      "http://#{request.host_with_port}/#{name}.git"
+      "https://#{request.host_with_port}/#{name}.git"
     end
   end
 end
