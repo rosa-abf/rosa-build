cadd1995d1f984b522aeddb95dd66ecc142825f3
Alexander Machehin <alexander.machehin@gmail.com>
2012-03-01 01:50:08 +0600

    [refs #230] fix forgot password SignUp style

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rosa::Application.configure do
   config.assets.digest = true
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w(login.css login.js registration.css)
+  config.assets.precompile += %w(login.css login.js reg_session.css)
 end
 
 # require 'stub_xml_rpc'
