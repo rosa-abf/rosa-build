7e53a6c511ebab7aaf660903de489cf323615de2
konstantin.grabar <yaroslav@markin.net>
2012-03-06 18:57:05 +0400

    [refs #265] Fix git push and delete branch partials and logic

--- app/models/activity_feed_observer.rb
+++ app/models/activity_feed_observer.rb
@@ -68,17 +68,18 @@ class ActivityFeedObserver < ActiveRecord::Observer
       project = Project.find_by_name(record.repo)
 
       last_commits = project.git_repository.repo.log(branch_name, nil).first(3)
-      first_commiter = User.find_by_email(last_commits[0].author.email) if last_commits.size > 0
+      first_commiter = User.find_by_email(last_commits[0].author.email) unless last_commits.blank?
       last_commits = last_commits.collect do |commit| #:author => 'author'
         [commit.sha, commit.message]
       end
 
       if change_type == 'delete'
         kind = 'git_delete_branch_notification'
-        options = {:user_id => first_commiter.id, :user_name => first_commiter.name, :user_email => first_commiter.email, :project_id => project.id, :project_name => project.name, :branch_name => branch_name, :change_type => change_type}
+        options = {:project_id => project.id, :project_name => project.name, :branch_name => branch_name, :change_type => change_type}
       else
         kind = 'git_new_push_notification'
         options = {:project_id => project.id, :project_name => project.name, :last_commits => last_commits, :branch_name => branch_name, :change_type => change_type}
+        options.merge!({:user_id => first_commiter.id, :user_name => first_commiter.name, :user_email => first_commiter.email}) if first_commiter
       end
 
       project.owner_and_admin_ids.each do |recipient|
