3e344f90adce11eea7ddb2056adcefac571aedd5
Pavel Chipiga <pavel.chipiga@gmail.com>
2012-03-12 00:47:22 +0200

    Display only 10 last activities. Refs #265

--- app/controllers/activity_feeds_controller.rb
+++ app/controllers/activity_feeds_controller.rb
@@ -2,6 +2,6 @@ class ActivityFeedsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @activity_feeds = current_user.activity_feeds.order('created_at DESC')
+    @activity_feeds = current_user.activity_feeds.order('created_at DESC').limit(10)
   end
 end
