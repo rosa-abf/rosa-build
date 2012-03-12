24639b8684bfedb61e6b93c6edc3fe5d4f114ffa
Alexander Machehin <alexander.machehin@gmail.com>
2012-03-12 21:03:25 +0600

    [refs 257] renamed to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,8 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def edit
+  def profile
+    @user ||= current_user
   end
 
   def create
@@ -51,11 +52,11 @@ class UsersController < ApplicationController
         @user.save
       end
       flash[:notice] = t('flash.user.saved')
-      redirect_to edit_user_path(@user)
+      redirect_to @user == current_user ? edit_profile_path : edit_user_path(@user)
     else
       flash[:error] = t('flash.user.save_error')
       flash[:warning] = @user.errors.full_messages.join('. ')
-      render(:action => :edit)
+      render(:action => :profile)
     end
   end
 
