843d95b64e5e3c36eb4ae1bd66a916b3c4b9334d
Alexander Machehin <alexander.machehin@gmail.com>
2012-03-12 23:02:02 +0600

    [refs #257] fixed 'big' geometry

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
     { :micro => { :geometry => "16x16",  :format => :jpg, :convert_options => '-strip -background white -flatten -quality 70'},
        :small => { :geometry => "30x30>",  :format => :jpg, :convert_options => '-strip -background white -flatten -quality 70'},
        :medium => { :geometry => "40x40>",  :format => :jpg, :convert_options => '-strip -background white -flatten -quality 70'},
-       :big => { :geometry => "81x81",  :format => :jpg, :convert_options => '-strip -background white -flatten -quality 70'}
+       :big => { :geometry => "81x81>",  :format => :jpg, :convert_options => '-strip -background white -flatten -quality 70'}
     }
   validates_inclusion_of :avatar_file_size, :in => (0..MAX_AVATAR_SIZE), :allow_nil => true
 
