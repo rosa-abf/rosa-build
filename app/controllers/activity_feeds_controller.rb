4addff059b7d20a17c31f2886539d771169a71ee
Alexander Machehin <alexander.machehin@gmail.com>
2012-03-15 23:52:54 +0600

    [refs #265] added activity feed tabs

--- app/controllers/activity_feeds_controller.rb
+++ app/controllers/activity_feeds_controller.rb
@@ -2,6 +2,10 @@ class ActivityFeedsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @activity_feeds = current_user.activity_feeds.order('created_at DESC').limit(10)
+    @filter = t('feed_menu').has_key?(params[:filter].to_sym) ? params[:filter].to_sym : :all
+    @activity_feeds = current_user.activity_feeds
+    @activity_feeds = @activity_feeds.where(:kind => "ActivityFeed::#{@filter.upcase}".constantize) unless @filter == :all
+
+    @activity_feeds = @activity_feeds.paginate :page => params[:page]
   end
 end
--- app/models/activity_feed.rb
+++ app/models/activity_feed.rb
@@ -1,9 +1,22 @@
 class ActivityFeed < ActiveRecord::Base
-  belongs_to :user
 
+  CODE = ['git_delete_branch_notification', 'git_new_push_notification', 'new_comment_commit_notification']
+  TRACKER = ['issue_assign_notification', 'new_comment_notification', 'new_issue_notification']
+  BUILD = []
+  WIKI = ['wiki_new_commit_notification']
+
+  belongs_to :user
   serialize :data
+
+  default_scope order('created_at DESC')
+
+  self.per_page = 10
 
   def partial
     'activity_feeds/partials/' + self.kind
+  end
+
+  def self.last_messages?(relation, page)
+    true #relation.count <= ActivityFeed::PER_PAGE * page
   end
 end
