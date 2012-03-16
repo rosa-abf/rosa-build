dd5f51de71de7b851e013d94442a84ef4592e8d0
Alexander Machehin <alexander.machehin@gmail.com>
2012-03-16 21:37:58 +0600

    [refs #265] added build tasks feed

--- app/controllers/activity_feeds_controller.rb
+++ app/controllers/activity_feeds_controller.rb
@@ -2,7 +2,7 @@ class ActivityFeedsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @filter = t('feed_menu').has_key?(params[:filter].to_sym) ? params[:filter].to_sym : :all
+    @filter = t('feed_menu').has_key?(params[:filter].try(:to_sym)) ? params[:filter].to_sym : :all
     @activity_feeds = current_user.activity_feeds
     @activity_feeds = @activity_feeds.where(:kind => "ActivityFeed::#{@filter.upcase}".constantize) unless @filter == :all
 
