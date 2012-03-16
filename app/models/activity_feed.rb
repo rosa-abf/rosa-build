fb89eedca322e770172e10bd557368833bd939f6
Alexander Machehin <alexander.machehin@gmail.com>
2012-03-17 00:58:41 +0600

    [refs #265] forgot build list setup

--- app/models/activity_feed.rb
+++ app/models/activity_feed.rb
@@ -2,7 +2,7 @@ class ActivityFeed < ActiveRecord::Base
 
   CODE = ['git_delete_branch_notification', 'git_new_push_notification', 'new_comment_commit_notification']
   TRACKER = ['issue_assign_notification', 'new_comment_notification', 'new_issue_notification']
-  BUILD = []
+  BUILD = ['build_list_notification']
   WIKI = ['wiki_new_commit_notification']
 
   belongs_to :user
