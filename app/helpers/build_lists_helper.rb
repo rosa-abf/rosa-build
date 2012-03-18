4b26eebeca955d1a8d3fc1afd48893ab6af26a8f
konstantin.grabar <yaroslav@markin.net>
2012-03-18 12:20:31 +0400

    [refs #223] Add new design to build list task page

---
+++ app/helpers/build_lists_helper.rb
@@ -0,0 +1,24 @@
+module BuildListsHelper
+  def build_list_status_color(status)
+    if [BuildList::BUILD_PUBLISHED, BuildServer::SUCCESS].include? status
+      return 'green'
+    end
+    if [BuildServer::BUILD_ERROR, BuildServer::PLATFORM_NOT_FOUND,
+        BuildServer::PROJECT_NOT_FOUND,BuildServer::PROJECT_VERSION_NOT_FOUND, BuildList::FAILED_PUBLISH].include? status
+      return 'red'
+    end
+
+    'nocolor'
+  end
+
+  def build_list_item_status_color(status)
+    if BuildServer::SUCCESS == status
+      return 'success'
+    end
+    if [BuildServer::DEPENDENCIES_ERROR, BuildServer::BUILD_ERROR, BuildList::Item::GIT_ERROR].include? status
+      return 'error'
+    end
+
+    ''
+  end
+end
