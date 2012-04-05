42aaa7c27a67e571ba8f62cfecedc087478bb9aa
Alexander Machehin <alexander.machehin@gmail.com>
2012-04-06 00:06:24 +0600

    [refs #295] fixed restarting DJ

--- lib/recipes/delayed_job.rb
+++ lib/recipes/delayed_job.rb
@@ -29,7 +29,9 @@ Capistrano::Configuration.instance(:must_exist).load do
 
     desc "Restart the delayed_job process"
     task :restart, :roles => lambda { roles } do
-      run "cd #{current_path};#{rails_env} script/delayed_job --queue=#{queue} -p #{queue} --pid-dir=#{current_path}/tmp/#{queue}_pids restart"
+      dj_queues.each do |queue|
+        run "cd #{current_path};#{rails_env} script/delayed_job --queue=#{queue} -p #{queue} --pid-dir=#{current_path}/tmp/#{queue}_pids restart"
+      end
     end
   end
 end
