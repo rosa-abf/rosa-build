89756ded0c8b862a4d6893728f4c41733c73312a
Vladimir Sharshov <vsharshov@gmail.com>
2012-04-13 21:38:57 +0400

    [Refs #223] Destroy non compatibility ActiveFeeds

--- db/migrate/20120413102757_build_average_time.rb
+++ db/migrate/20120413102757_build_average_time.rb
@@ -3,4 +3,7 @@ class BuildAverageTime < ActiveRecord::Migration
   	add_column :projects, :average_build_time, :integer, :null => false, :default => 0
     add_column :projects, :build_count, :integer, :null => false, :default => 0
   end
+
+  ActivityFeed.where(:kind => 'build_list_notification').destroy_all
+
 end
