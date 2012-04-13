36447051ec1cd063c77625268b788a9396d8184f
Vladimir Sharshov <vsharshov@gmail.com>
2012-04-13 13:48:57 +0400

    [Refs #223] Small refactoring for pull request

--- db/migrate/20120411122636_add_started_at_and_finished_at_to_build_lists.rb
+++
@@ -1,6 +0,0 @@
-class AddStartedAtAndFinishedAtToBuildLists < ActiveRecord::Migration
-  def change
-    add_column :build_lists, :started_at, :datetime
-    add_column :build_lists, :finished_at, :datetime
-  end
-end
