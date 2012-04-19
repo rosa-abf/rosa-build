2fc4e2db61ffdcd54df9eada9fffea3754c80120
konstantin.grabar <santaux@gmail.com>
2012-04-19 14:50:43 +0400

    [refs #396] Fix token migration. Fix atom link tag title

--- db/migrate/20120418100619_add_token_authenticatable_to_users.rb
+++ db/migrate/20120418100619_add_token_authenticatable_to_users.rb
@@ -1,13 +1,15 @@
 class AddTokenAuthenticatableToUsers < ActiveRecord::Migration
-  def change
-    change_table :users do |t|
-      t.token_authenticatable
-    end
-
+  def self.up
+    add_column :users, :authentication_token, :string
     add_index :users, :authentication_token
 
     User.all.each do |user|
       user.ensure_authentication_token!
     end
+  end
+
+  def self.down
+    remove_column :users, :authentication_token
+    remove_index :users, :authentication_token
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -350,6 +350,7 @@ ActiveRecord::Schema.define(:version => 20120418100619) do
     t.string   "authentication_token"
   end
 
+  add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token"
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
