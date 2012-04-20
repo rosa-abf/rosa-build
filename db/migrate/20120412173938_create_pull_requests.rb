25bd2a6e7795bd82daaa8d938746aad71a3dcb7c
Alexander Machehin <alexander.machehin@gmail.com>
2012-04-20 23:19:23 +0600

    [refs #90] issue status -> state

--- db/migrate/20120412173938_create_pull_requests.rb
+++ db/migrate/20120412173938_create_pull_requests.rb
@@ -1,7 +1,7 @@
 class CreatePullRequests < ActiveRecord::Migration
   def change
     add_column :issues, :type, :string
-    add_column :issues, :state, :string
+    rename_column :issues, :status, :state
     add_column :issues, :data, :text, :null => false, :default => 0
   end
 end
