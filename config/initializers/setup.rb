0ca6c76ce2b6597de4a66698fffcbfacf977129b
konstantin.grabar <santaux@gmail.com>
2012-04-23 16:53:30 +0400

    [refs #417] Make changes for devise action mailer host set

--- config/initializers/setup.rb
+++ config/initializers/setup.rb
@@ -10,3 +10,5 @@ config = {:project_root => File.join(APP_CONFIG['root_path'], 'git_projects'), :
 # Rosa::Application.config.middleware.insert_before 0, ::Grack::Handler, config
 Rosa::Application.config.middleware.insert_after ::Rails::Rack::Logger, ::Grack::Handler, config
 Rosa::Application.config.middleware.insert_before ::Grack::Handler, ::Grack::Auth
+
+Rosa::Application.config.action_mailer.default_url_options = { :host => APP_CONFIG['action_mailer_host'] } if APP_CONFIG['action_mailer_host']
