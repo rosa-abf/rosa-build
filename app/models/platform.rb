4f38bb53648fc3f125a5bd8234a7666c9da8355f
konstantin.grabar <santaux@gmail.com>
2012-05-16 20:58:15 +0400

    [refs #438] Remove symlink dir creation

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -135,7 +135,6 @@ class Platform < ActiveRecord::Base
 
   def symlink_directory
     # umount_directory_for_rsync # TODO ignore errors
-    system("mkdir -p -m 0777 #{symlink_path}")
     system("ln -s #{path} #{symlink_path}")
     Arch.all.each do |arch|
       str = "country=Russian Federation,city=Moscow,latitude=52.18,longitude=48.88,bw=1GB,version=2011,arch=#{arch.name},type=distrib,url=#{public_downloads_url}\n"
