7561abca081a92f94518a4b784a7ef4004cfe70e
Vladimir Sharshov <vsharshov@gmail.com>
2012-06-28 21:40:32 +0400

    [refs #462] refactoring: remove notified_at

--- app/controllers/platforms/product_build_lists_controller.rb
+++ app/controllers/platforms/product_build_lists_controller.rb
@@ -11,14 +11,13 @@ class Platforms::ProductBuildListsController < Platforms::BaseController
   before_filter :find_product_build_list, :only => [:status_build]
 
   def create
-    @product.product_build_lists.create! :base_url => "http://#{request.host_with_port}", :notified_at => Time.current
+    @product.product_build_lists.create! :base_url => "http://#{request.host_with_port}"
     flash[:notice] = t('flash.product.build_started')
     redirect_to [@platform, @product]
   end
 
   def status_build
     @product_build_list.status = params[:status].to_i # ProductBuildList::BUILD_COMPLETED : ProductBuildList::BUILD_FAILED)
-    @product_build_list.notified_at = Time.current
     @product_build_list.save!
     render :nothing => true
   end
