8c88e7a0da4174f52e0b817ba9df3c9e6713d3fb
George Vinogradov <g.vinogradov@itima.ru>
2012-07-04 15:42:43 +0400

    [issue #428] Some code simplification.

--- app/controllers/advisories_controller.rb
+++ app/controllers/advisories_controller.rb
@@ -26,8 +26,7 @@ class AdvisoriesController < ApplicationController
   end
 
   def search
-    puts params[:bl_type]
-    @advisory = Advisory.by_update_type(params[:bl_type]).search_by_id(params[:query]).limit(1).first
+    @advisory = Advisory.by_update_type(params[:bl_type]).search_by_id(params[:query]).first
     raise ActionController::RoutingError.new('Not Found') if @advisory.nil?
     respond_to do |format|
       format.json { render @advisory }
