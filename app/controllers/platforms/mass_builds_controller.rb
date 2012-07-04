82612c368dd80e0ab4969aa6c4e614c14bec1802
konstantin.grabar <santaux@gmail.com>
2012-07-04 16:15:12 +0400

    [refs #442] Move mass_builds to platforms dir

--- app/controllers/platforms/mass_builds_controller.rb
+++ app/controllers/platforms/mass_builds_controller.rb
@@ -1,4 +1,5 @@
-class MassBuildsController < ApplicationController
+#class MassBuildsController < ApplicationController
+class Platforms::MassBuildsController < Platforms::BaseController
   before_filter :authenticate_user!
 
   load_and_authorize_resource :platform
