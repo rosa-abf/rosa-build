ec7d132a3f18d2c1ba3c795b6ef77073b3df60a3
Alexander Machehin <alexander.machehin@gmail.com>
2012-08-08 23:57:03 +0600

    [refs #90] fixed update commits button

--- app/assets/javascripts/extra/pull.js
+++ app/assets/javascripts/extra/pull.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
+  var upd_action = $('#update_action').val();
+  var form = $('#new_pull_request');
+
   $('#pull_request_base_project').on('autocompleteselect', function(event, data){
     $('input#base_refs').autocomplete('option', 'source', data.item.refs);
   });
 
   $('#pull_request_base_project, input#base_refs, input#head_refs').on('autocompleteselect', function(event, data){
-    var tmp = $('#update_action').val();
-    $('#new_pull_request').attr('action', $('#update_action').val());
+    form.attr('action', upd_action);
+    form.attr('method', 'get');
     $('#update_pull').fadeIn('fast');
     $('#create_pull').fadeOut('fast');
   });
