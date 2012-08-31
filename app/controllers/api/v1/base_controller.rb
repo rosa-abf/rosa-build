6229daf137f37d24d0ef3f7f87516babe66221a6
konstantin.grabar <santaux@gmail.com>
2012-08-31 16:23:44 +0400

    [refs #374] Add paginate restriction for API

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,3 +1,11 @@
 # -*- encoding : utf-8 -*-
 class Api::V1::BaseController < ApplicationController
+  before_filter :restrict_paginate, :only => :index
+
+  protected
+
+  def restrict_paginate
+    params[:per_page] = 30  if params[:per_page].blank? or params[:per_page].to_i < 1
+    params[:per_page] = 100 if params[:per_page].to_i >100
+  end
 end
