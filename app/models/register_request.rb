bff32c27c4627887803075287baed363ac35fe93
Vokhmin Alexey V <avokhmin@gmail.com>
2012-09-11 12:36:33 +0400

    #637: removed comments

--- app/models/register_request.rb
+++ app/models/register_request.rb
@@ -1,51 +1,35 @@
 # -*- encoding : utf-8 -*-
 class RegisterRequest < ActiveRecord::Base
 
-  #---------------
-  # *** Scopes ***
-  #+++++++++++++++
-
   default_scope order('created_at ASC')
 
   scope :rejected, where(:rejected => true)
   scope :approved, where(:approved => true)
   scope :unprocessed, where(:approved => false, :rejected => false)
 
-  #--------------------
-  # *** Validations ***
-  #++++++++++++++++++++
-
   validates :email, :presence => true, :uniqueness => {:case_sensitive => false}, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
-
-  #------------------
-  # *** Callbacks ***
-  #++++++++++++++++++
 
   # before_create :generate_token
   before_update :invite_approve_notification
 
-  #-------------------------
-  # *** Instance Methods ***
-  #+++++++++++++++++++++++++
-
   def approve
     update_attributes(:approved => true, :rejected => false)
-  end # approve
+  end
 
   def reject
     update_attributes(:approved => false, :rejected => true)
-  end # reject
+  end
 
   protected
 
   def generate_token
     self.token = Digest::SHA1.hexdigest(name + email + Time.now.to_s + rand.to_s)
-  end # generate_token
+  end
 
   def invite_approve_notification
     if approved_changed? && approved?
       generate_token
       UserMailer.invite_approve_notification(self).deliver
     end
-  end # invite_approve_notification
-end # RegisterRequest
+  end
+end
