e3bcd2382e39f12e8fb26f3a0800d05b6d04a1c6
Vladimir Sharshov <vsharshov@gmail.com>
2012-09-12 18:49:40 +0400

    [refs #641] Show only public projects in group profile

--- app/controllers/users/profile_controller.rb
+++ app/controllers/users/profile_controller.rb
@@ -3,8 +3,6 @@ class Users::ProfileController < Users::BaseController
   autocomplete :user, :uname
 
   def show
-    @groups = @user.groups.uniq
-    @platforms = @user.platforms.paginate(:page => params[:platform_page], :per_page => 10)
-    @projects = @user.projects.paginate(:page => params[:project_page], :per_page => 10)
+    @projects = @user.projects.by_visibilities(['open'])
   end
 end
