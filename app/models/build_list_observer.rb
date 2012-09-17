8788db12c0dbcf2d143ce786fc273e104038ad4c
Vokhmin Alexey V <avokhmin@gmail.com>
2012-09-17 19:18:02 +0400

    #575: added notification for users

--- app/models/build_list_observer.rb
+++ app/models/build_list_observer.rb
@@ -1,4 +1,7 @@
 class BuildListObserver < ActiveRecord::Observer
+  PUBLICATION_STATUSES  = [BuildList::BUILD_PUBLISHED, BuildList::FAILED_PUBLISH]
+  STATUSES = [BuildServer::BUILD_ERROR, BuildServer::SUCCESS] + PUBLICATION_STATUSES
+
   observe :build_list
 
   def before_update(record)
@@ -15,6 +18,33 @@ class BuildListObserver < ActiveRecord::Observer
           record.project.update_attributes({ :average_build_time => new_av_time, :build_count => build_count + 1 }, :without_protection => true)
         end
       end
+      BuildListObserver.notify_users(record)
     end
-  end
+  end # before_update
+
+  private
+
+  def self.notify_users(build_list)
+    unless build_list.mass_build_id
+      users = []
+      if build_list.project # find associated users
+        associated_users = build_list.project.members
+        associated_users << (build_list.project.owner.is_a?(User) ?
+          build_list.project.owner : build_list.project.owner.members)
+        users = associated_users.flatten.uniq.
+          select{ |user| user.notifier.can_notify? && user.notifier.new_associated_build? }
+      end
+      if build_list.user.notifier.can_notify? && build_list.user.notifier.new_build?
+        users << build_list.user unless users.include?(build_list.user)
+      end
+      users.each do |user|
+        if (( build_list.auto_publish? &&
+              PUBLICATION_STATUSES.include?(build_list.status) ) ||
+            ( !build_list.auto_publish? && STATUSES.include?(build_list.status) ))
+
+            UserMailer.build_list_notification(build_list, user).deliver
+        end
+      end
+    end
+  end # notify_users
 end
