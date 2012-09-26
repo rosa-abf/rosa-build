5f0e6837118a13a4cf8794b9347a628f554d7439
Vokhmin Alexey V <avokhmin@gmail.com>
2012-09-26 19:41:00 +0400

    #90: moved code

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -31,8 +31,7 @@ class Issue < ActiveRecord::Base
   # Using mb_chars for correct transform to lowercase ('Русский Текст'.downcase => "Русский Текст")
   scope :search, lambda {|q| where('issues.title ILIKE ?', "%#{q.mb_chars.downcase}%") if q.present?}
   scope :def_order, order('issues.serial_id desc')
-  scope :without_pull_requests,
-    where('NOT EXISTS (select null from pull_requests as pr where pr.issue_id = issues.id)')
+  scope :without_pull_requests, where('NOT EXISTS (select null from pull_requests as pr where pr.issue_id = issues.id)')
 
   def assign_uname
     assignee.uname if assignee
