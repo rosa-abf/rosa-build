47046fdd7cf96fde39d1f9ff45c41192acee4b12
Alexander Machehin <alexander.machehin@gmail.com>
2012-10-03 19:00:09 +0600

    [refs #90] fixed migration for ActiveRecord::IrreversibleMigration

--- db/migrate/20121003081546_rename_base_head_in_pull_requests.rb
+++ db/migrate/20121003081546_rename_base_head_in_pull_requests.rb
@@ -1,10 +1,8 @@
 class RenameBaseHeadInPullRequests < ActiveRecord::Migration
   def change
-    change_table :pull_requests do |t|
-      t.rename :base_project_id, :to_project_id
-      t.rename :base_ref, :to_ref
-      t.rename :head_project_id, :from_project_id
-      t.rename :head_ref, :from_ref
-    end
+    rename_column :pull_requests, :base_project_id, :to_project_id
+    rename_column :pull_requests, :base_ref, :to_ref
+    rename_column :pull_requests, :head_project_id, :from_project_id
+    rename_column :pull_requests, :head_ref, :from_ref
   end
 end
