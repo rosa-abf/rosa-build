df0251282d5cb7350a5c473d292575bf3bbc464f
Vokhmin Alexey V <avokhmin@gmail.com>
2012-10-04 17:13:55 +0400

    #671: updated API arches controller, added specs

--- app/controllers/api/v1/arches_controller.rb
+++ app/controllers/api/v1/arches_controller.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Api::V1::ArchesController < Api::V1::BaseController
-  skip_before_filter :authenticate_user!, :only => [:index] if APP_CONFIG['anonymous_access']
+  before_filter :authenticate_user! unless APP_CONFIG['anonymous_access']
 
   def index
     @arches = Arch.order(:id).all
