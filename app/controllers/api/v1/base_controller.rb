992892f2c8896a9d186ea09a18396cd292017f3c
Vokhmin Alexey V <avokhmin@gmail.com>
2012-10-11 19:18:54 +0400

    #671: small refactoring

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,5 @@
 # -*- encoding : utf-8 -*-
 class Api::V1::BaseController < ApplicationController
-  
   #respond_to :json
 
   rescue_from CanCan::AccessDenied do |exception|
@@ -16,6 +15,25 @@ class Api::V1::BaseController < ApplicationController
     per_page = 20 if per_page < 1
     per_page = 100 if per_page >100
     {:page => params[:page], :per_page => per_page}
+  end
+
+  def render_json_response(message, status = 200)
+    id = status != 200 ? nil : @subject.id
+
+    render :json => {
+      @subject.class.name.downcase.to_sym => {
+        :id => id,
+        :message => message
+      }
+    }.to_json, :status => status
+  end
+
+  def render_validation_error(message)
+    errors = @subject.errors.full_messages.join('. ')
+    if errors.present?
+      message << '. ' << errors
+    end
+    render_json_response(message, 422)
   end
 
 end
--- app/controllers/api/v1/platforms_controller.rb
+++ app/controllers/api/v1/platforms_controller.rb
@@ -1,10 +1,10 @@
 # -*- encoding : utf-8 -*-
 class Api::V1::PlatformsController < Api::V1::BaseController
-
   before_filter :authenticate_user!
   skip_before_filter :authenticate_user!, :only => [:show, :platforms_for_build, :members] if APP_CONFIG['anonymous_access']
 
   load_and_authorize_resource
+  before_filter :set_subject, :except => [:index, :platforms_for_build, :show, :members]
 
   def index
     @platforms = @platforms.accessible_by(current_ability, :related).
@@ -24,9 +24,9 @@ class Api::V1::PlatformsController < Api::V1::BaseController
     owner = User.where(:id => platform_params[:owner_id]).first
     @platform.owner = owner || get_owner
     if @platform.save
-      render :json => json_response('Platform has been created successfully')
+      render_json_response 'Platform has been created successfully'
     else
-      render :json => json_response('Platform has not been created', true), :status => 422
+      render_validation_error 'Platform has not been created'
     end
   end
 
@@ -35,9 +35,9 @@ class Api::V1::PlatformsController < Api::V1::BaseController
     owner = User.where(:id => platform_params[:owner_id]).first
     platform_params[:owner] = owner if owner
     if @platform.update_attributes(platform_params)
-      render :json => json_response('Platform has been updated successfully')
+      render_json_response 'Platform has been updated successfully'
     else
-      render :json => json_response('Platform has not been updated', true), :status => 422
+      render_validation_error 'Platform has not been updated'
     end
   end
 
@@ -47,17 +47,17 @@ class Api::V1::PlatformsController < Api::V1::BaseController
 
   def add_member
     if member.present? && @platform.add_member(member)
-      render :json => json_response("#{member.class.to_s} '#{member.id}' has been added to platform successfully")
+      render_json_response "#{member.class.to_s} '#{member.id}' has been added to platform successfully"
     else
-      render :json => json_response('Member has not been added to platform', true), :status => 422
+      render_validation_error 'Member has not been added to platform'
     end
   end
 
   def remove_member
     if member.present? && @platform.remove_member(member)
-      render :json => json_response("#{member.class.to_s} '#{member.id}' has been removed from platform successfully")
+      render_json_response "#{member.class.to_s} '#{member.id}' has been removed from platform successfully"
     else
-      render :json => json_response('Member has not been removed from platform'), :status => 422
+      render_validation_error 'Member has not been removed from platform'
     end
   end
 
@@ -66,36 +66,23 @@ class Api::V1::PlatformsController < Api::V1::BaseController
     platform_params[:owner] = current_user
     @cloned = @platform.full_clone(platform_params)
     if @cloned.persisted?
-      render :json => json_response('Platform has been cloned successfully')
+      render_json_response 'Platform has been cloned successfully'
     else
-      render :json => json_response('Platform has not been cloned', true), :status => 422
+      render_validation_error 'Platform has not been cloned'
     end
   end
 
   def clear
     @platform.clear
-    render :json => json_response('Platform has been cleared successfully')
+    render_json_response 'Platform has been cleared successfully'
   end
 
   def destroy
     @platform.destroy # later with resque
-    render :json => json_response('Platform has been destroyed successfully')
+    render_json_response 'Platform has been destroyed successfully'
   end
 
   private
-
-  def json_response(message, nullify_id = false)
-    if nullify_id
-      errors = @platform.errors.full_messages.join('. ')
-      if errors.present?
-        message << '. ' << errors
-      end
-      id = nil
-    else
-      id = @platform.id
-    end
-    { :platform => {:id => id, :message => message} }.to_json
-  end
 
   def member
     return @member if @member
@@ -106,6 +93,10 @@ class Api::V1::PlatformsController < Api::V1::BaseController
     end
     @member = member.where(:id => params[:member_id]).first if member
     @member ||= ''
+  end
+
+  def set_subject
+    @subject = @platform
   end
 
 end
