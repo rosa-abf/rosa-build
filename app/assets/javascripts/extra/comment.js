9bc9eab4f161acccf36f5521b228a040c16d691f
Alexander Machehin <alexander.machehin@gmail.com>
2012-10-19 18:39:11 +0600

    [refs #579] remove new comment form after close & fix focus

--- app/assets/javascripts/extra/comment.js
+++ app/assets/javascripts/extra/comment.js
@@ -43,12 +43,15 @@ $(document).ready(function() {
       if(yield) {
         var str = "<tr class='inline-comments'><td class='line_numbers' colspan='2'></td>"+"<td>"+data+"</td></tr>";
         par.after(str);
+        par = par.next();
       }
       else {
         par.find('td:last').append(data);
       }
-      $("a[href='"+line.attr('href')+"']").addClass('new_comment_exists');
-      par.parent().find('#md_tabs.nav.nav-tabs').each(function(i) { $(this).find('a:first').tab('show') });
+      par.find('#md_tabs.nav.nav-tabs').each(function(i) {
+        $(this).find('a:first').tab('show');
+        $(this).parent().find('#new_line_edit_input').focus();
+      });
     }
     var line = $(this);
     var tmp = line.parents('tr');
@@ -70,40 +73,27 @@ $(document).ready(function() {
     // Hide visible new comment form
     $('#open-comment.new_line_comment').parents('.inline-comments').each(function(i) {
       if($(this).find('.line-comments').length > 0) {
-        $(this).find('#open-comment.new_line_comment').hide();
+        $(this).find('#open-comment.new_line_comment').remove();
         $(this).find('.new_inline_comment.button').show();
       }
       else {
-        $(this).hide();
+        $(this).remove();
       }
     });
     par.find('.new_inline_comment.button').hide();
 
-    // Show existing new comment form or load them.
-    if(line.hasClass('new_comment_exists')) {
-      var tr = line.parents('tr').next();
-      if(tr.find('.line-comments').length > 0) {
-        tr.find('#open-comment.new_line_comment').show();
-      }
-      else {
-        tr.show();
-      }
-    }
-    else {
-      $.get(line.attr('href'), null, ProcessData);
-    }
-    $('#new_line_edit_input').focus();
+    $.get(line.attr('href'), null, ProcessData);
     return false;
   });
 
   $(document).on('click', '.cancel_inline_comment.button', function() {
     var tr = $(this).parents('.inline-comments');
     if(tr.find('.line-comments').length > 0) {
-      tr.find('#open-comment.new_line_comment').hide();
+      tr.find('#open-comment.new_line_comment').remove();
       tr.find('.new_inline_comment.button').show();
     }
     else {
-      tr.hide();
+      tr.remove();
     }
     return false;
   });
