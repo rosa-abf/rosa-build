e64398f82b9e31434f2299c9ea38c2a2dc62afe5
Alexander Machehin <alexander.machehin@gmail.com>
2012-11-02 23:21:28 +0600

    [file-store-4] add rosa_system user for file-store

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,6 @@ ARCHES = %w(i386 i586 x86_64)
 ARCHES.each do |arch|
   Arch.find_or_create_by_name arch
 end
+
+user = User.new uname: 'rosa_system', email: 'rosa_system@rosalinux.ru', password: SecureRandom.base64
+user.confirmed_at = Time.now.utc; user.save
