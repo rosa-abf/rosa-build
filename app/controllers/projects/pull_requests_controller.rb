8df87ed2928d7114f7f59187800cf9351ddacf1e
Alexander Machehin <alexander.machehin@gmail.com>
2012-11-14 16:51:10 +0600

    [refs #90] fixed error on create

--- app/controllers/projects/pull_requests_controller.rb
+++ app/controllers/projects/pull_requests_controller.rb
@@ -38,6 +38,8 @@ class Projects::PullRequestsController < Projects::BaseController
 
     @pull = to_project.pull_requests.new pull_params
     @pull.issue.user, @pull.issue.project, @pull.from_project = current_user, to_project, @project
+    @pull.from_project_owner_uname = @pull.from_project.owner.uname
+    @pull.from_project_name = @pull.from_project.name
 
     if @pull.valid? # FIXME more clean/clever logics
       @pull.save # set pull id
