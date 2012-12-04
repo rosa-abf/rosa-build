0a88010a8315c57f6cb444104910405a7bf641c7
Alexander Machehin <alexander.machehin@gmail.com>
2012-12-04 17:12:51 +0600

    fix pagination

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -77,7 +77,7 @@ class Api::V1::BaseController < ApplicationController
     per_page = 20 if per_page < 1
     per_page = 100 if per_page >100
     page = params[:page].to_i
-    page = 1 if page == 0
+    page = nil if page == 0
     {:page => page, :per_page => per_page}
   end
 
