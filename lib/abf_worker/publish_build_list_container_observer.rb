e0a1abcd4c5ec4bde3868e820f8071da70c88f73
Vokhmin Alexey V <avokhmin@gmail.com>
2012-12-10 21:01:34 +0400

    #755: remove unnecessary weorkers, add Observer

---
+++ lib/abf_worker/publish_build_list_container_observer.rb
@@ -0,0 +1,10 @@
+module AbfWorker
+  class PublishBuildListContainerObserver
+    @queue = :publish_build_list_container_observer
+
+    def self.perform(options)
+      # TODO: ...
+    end
+
+  end
+end
