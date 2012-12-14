0fec0fb18426ec092bc5d51cee3d17503ff4cfb0
Vokhmin Alexey V <avokhmin@gmail.com>
2012-12-14 21:23:41 +0400

    #778: remove unnecessary code

--- lib/abf_worker/file_system_worker.rb
+++ lib/abf_worker/file_system_worker.rb
@@ -5,8 +5,6 @@ module AbfWorker
     def self.perform(options)
       id, action = options['id'], options['action']
       case options['type']
-      when 'platform'
-        @runner = AbfWorker::Runners::Platform.new id, action
       when 'repository'
         @runner = AbfWorker::Runners::Repository.new id, action
       end
