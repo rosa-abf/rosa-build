a1444cad6b1b7af86e6db81a085a3479d033b3c0
Vokhmin Alexey V <avokhmin@gmail.com>
2013-01-23 13:42:05 +0400

    hot fix for PublishObserver

--- lib/abf_worker/publish_observer.rb
+++ lib/abf_worker/publish_observer.rb
@@ -3,36 +3,36 @@ module AbfWorker
     @queue = :publish_observer
 
 
+    def self.perform(options)
+      status = options['status'].to_i
+      return if status == STARTED # do nothing when publication started
+      if options['type'] == 'resign'
+        AbfWorker::BuildListsPublishTaskManager.unlock_repository options['id']
+      else
+        update_rpm_builds options
+      end
+    end
+
+    def self.update_rpm_builds(options)
+      build_lists = BuildList.where(:id => options['build_list_ids'])
+      build_lists.each do |bl| 
+        update_results(bl, options)
+        case status
+        when COMPLETED
+          bl.published
+          AbfWorker::BuildListsPublishTaskManager.cleanup_completed options['projects_for_cleanup']
+        when FAILED, CANCELED
+          bl.fail_publish
+          AbfWorker::BuildListsPublishTaskManager.cleanup_failed options['projects_for_cleanup']
+        end
+        AbfWorker::BuildListsPublishTaskManager.unlock_build_list bl
+      end
+      bl = build_lists.first || BuildList.find(options['id'])
+      AbfWorker::BuildListsPublishTaskManager.unlock_rep_and_platform bl
+    end
+
     class << self
-      def perform(options)
-        status = options['status'].to_i
-        return if status == STARTED # do nothing when publication started
-        if options['type'] == 'resign'
-          AbfWorker::BuildListsPublishTaskManager.unlock_repository options['id']
-        else
-          update_rpm_builds options
-        end
-      end
-
       protected
-
-      def update_rpm_builds(options)
-        build_lists = BuildList.where(:id => options['build_list_ids'])
-        build_lists.each do |bl| 
-          update_results(bl, options)
-          case status
-          when COMPLETED
-            bl.published
-            AbfWorker::BuildListsPublishTaskManager.cleanup_completed options['projects_for_cleanup']
-          when FAILED, CANCELED
-            bl.fail_publish
-            AbfWorker::BuildListsPublishTaskManager.cleanup_failed options['projects_for_cleanup']
-          end
-          AbfWorker::BuildListsPublishTaskManager.unlock_build_list bl
-        end
-        bl = build_lists.first || BuildList.find(options['id'])
-        AbfWorker::BuildListsPublishTaskManager.unlock_rep_and_platform bl
-      end
 
       def update_results(subject, options)
         results = (subject.results || []).
@@ -40,8 +40,6 @@ module AbfWorker
         results |= options['results']
         sort_results_and_save(subject, results)
       end
-
     end
-
   end
 end
