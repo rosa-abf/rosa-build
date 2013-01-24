e1b79a3c1f73b6887e8423ba08f10885b5e3a1c7
Vokhmin Alexey V <avokhmin@gmail.com>
2013-01-24 15:58:42 +0400

    #848: small refactoring

--- lib/abf_worker/status_inspector.rb
+++ lib/abf_worker/status_inspector.rb
@@ -21,14 +21,14 @@ module AbfWorker
         status = {}
         queues.each do |worker|
           workers = Resque.workers.select{ |w| yield w, worker }
-          fill_status status, workers, worker
+          status[worker] = status_of_worker workers, worker
         end
         status
       end
 
-      def fill_status(status, workers, worker)
+      def status_of_worker(workers, worker)
         redis, key = Resque.redis, "queue:#{worker}_worker"
-        status[worker] = {
+        {
           :count        => workers.count,
           :build_tasks  => workers.select{ |w| w.working? }.count,
           :tasks        => (redis.llen("#{key}_default") + redis.llen(key))
