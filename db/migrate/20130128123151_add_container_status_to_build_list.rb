77845adb51bd3665d20c102e850986fb4a88ef53
Vokhmin Alexey V <avokhmin@gmail.com>
2013-01-29 18:17:15 +0400

    #860: update migration

--- db/migrate/20130128123151_add_container_status_to_build_list.rb
+++ db/migrate/20130128123151_add_container_status_to_build_list.rb
@@ -1,7 +1,12 @@
 class AddContainerStatusToBuildList < ActiveRecord::Migration
+  class BuildList < ActiveRecord::Base
+    WAITING_FOR_RESPONSE = 4000
+  end
+
   def up
     add_column    :build_lists, :container_status, :integer
     remove_column :build_lists, :container_path
+    BuildList.where(:container_status => nil).update_all(:container_status => BuildList::WAITING_FOR_RESPONSE)
   end
 
   def down
