2a2385dc538012abd7c31bcb0149087033f28bcf
Vokhmin Alexey V <avokhmin@gmail.com>
2013-02-01 17:39:35 +0400

    #881: update migration

--- db/migrate/20130201094007_add_use_personal_repository_to_build_list.rb
+++ db/migrate/20130201094007_add_use_personal_repository_to_build_list.rb
@@ -1,5 +1,5 @@
 class AddUsePersonalRepositoryToBuildList < ActiveRecord::Migration
   def change
-    add_column :build_lists, :use_save_to_repository, :boolean
+    add_column :build_lists, :use_save_to_repository, :boolean, :default => false
   end
 end
