a3220d3bba1b3786ac3b9118e4df649565a2cbda
Vokhmin Alexey V <avokhmin@gmail.com>
2013-02-22 17:44:00 +0400

    #757: update help dialog

--- app/assets/javascripts/extra/build_list.js
+++ app/assets/javascripts/extra/build_list.js
@@ -54,8 +54,7 @@ $(document).ready(function() {
   $('#extra-repos-and-build-lists-dialog').dialog({
     autoOpen: false,
     resizable: false,
-    width: 400,
-    height: 130
+    width: 400
   });
 
   $('#extra-repos-and-build-lists .icon-question-sign').click(function() {
