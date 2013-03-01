d4b72a559cd2049b4f7ffec10fb954aa2f432c53
Alexander Machehin <alexander.machehin@gmail.com>
2013-03-02 01:33:11 +0600

    #953 fix running sudo

--- app/models/ssh_key.rb
+++ app/models/ssh_key.rb
@@ -2,7 +2,7 @@
 # https://github.com/gitlabhq/gitlabhq/blob/15c0e58a49d623a0f8747e1d7e74364324eeb79f/app/models/key.rb
 
 class SshKey < ActiveRecord::Base
-  SHELL_KEY_COMMAND = "sudo ~#{APP_CONFIG['shell_user']}/gitlab-shell/bin/gitlab-keys"
+  SHELL_KEY_COMMAND = "sudo -i -u git ~#{APP_CONFIG['shell_user']}/gitlab-shell/bin/gitlab-keys"
 
   belongs_to :user
   attr_accessible :key, :name
