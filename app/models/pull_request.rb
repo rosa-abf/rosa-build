2ae652d70b25697f7db95d219491995a5e2a8f7f
Alexander Machehin <alexander.machehin@gmail.com>
2013-03-07 18:01:08 +0600

    fixed updating pull code

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -177,7 +177,9 @@ class PullRequest < ActiveRecord::Base
 
     Dir.chdir(path) do
       system 'git', 'tag', '-d', from_ref, to_ref
-      system 'git fetch --tags'
+      system 'git fetch --tags -all'
+      system 'git fetch --all'
+
       tags, head = repo.tags.map(&:name), to_project == from_project ? 'origin' : 'head'
       system 'git', 'checkout', to_ref
       unless tags.include? to_ref
