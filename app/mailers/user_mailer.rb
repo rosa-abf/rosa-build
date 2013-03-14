8a145e4db74c5d3c57a40a186e571055b7d59452
Vokhmin Alexey V <avokhmin@gmail.com>
2013-03-14 19:31:13 +0400

    #22: some refactoring

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,18 @@
 # -*- encoding : utf-8 -*-
 
 class UserMailer < ActionMailer::Base
-  default :from => APP_CONFIG['do-not-reply-email']
+  default :from => "#{APP_CONFIG['project_name']} <#{APP_CONFIG['do-not-reply-email']}>"
   default_url_options.merge!(:protocol => 'https') if APP_CONFIG['mailer_https_url']
 
   include Resque::Mailer # send email async
 
   def new_user_notification(user)
     @user = user
-    mail(:to => email_with_name(user), :subject => I18n.t("notifications.subjects.new_user_notification", :project_name => APP_CONFIG['project_name'])) do |format|
+    mail(
+      :to           => email_with_name(user, user.email),
+      :subject      => I18n.t("notifications.subjects.new_user_notification",
+      :project_name => APP_CONFIG['project_name'])
+    ) do |format|
       format.html
     end
   end
@@ -18,9 +22,9 @@ class UserMailer < ActionMailer::Base
     subject = @comment.issue_comment? ? subject_for_issue(@comment.commentable) :
       I18n.t('notifications.subjects.new_commit_comment_notification')
     mail(
-      :to       => email_with_name(user),
+      :to       => email_with_name(user, user.email),
       :subject  => subject,
-      :from     => email_with_name(comment.user, false)
+      :from     => email_with_name(comment.user)
     ) do |format|
       format.html
     end
@@ -29,9 +33,9 @@ class UserMailer < ActionMailer::Base
   def new_issue_notification(issue, user)
     @user, @issue = user, issue
     mail(
-      :to       => email_with_name(user),
+      :to       => email_with_name(user, user.email),
       :subject  => subject_for_issue(issue, true),
-      :from     => email_with_name(issue.user, false)
+      :from     => email_with_name(issue.user)
     ) do |format|
       format.html
     end
@@ -39,7 +43,10 @@ class UserMailer < ActionMailer::Base
 
   def issue_assign_notification(issue, user)
     @user, @issue = user, issue
-    mail(:to => email_with_name(user), :subject => subject_for_issue(@issue)) do |format|
+    mail(
+      :to       => email_with_name(user, user.email),
+      :subject  => subject_for_issue(@issue)
+    ) do |format|
       format.html
     end
   end
@@ -53,9 +60,9 @@ class UserMailer < ActionMailer::Base
     subject << " - #{build_list.human_status} "
     subject << I18n.t("notifications.subjects.for_arch", :arch => @build_list.arch.name)
     mail(
-      :to       => email_with_name(user),
+      :to       => email_with_name(user, user.email),
       :subject  => subject,
-      :from     => email_with_name(build_list.user, false)
+      :from     => email_with_name(build_list.user)
     ) do |format|
       format.html
     end
@@ -64,15 +71,18 @@ class UserMailer < ActionMailer::Base
   def invite_approve_notification(register_request)
     I18n.locale = register_request.language if register_request.language
     @register_request = register_request
-    mail :to => register_request.email, :subject => I18n.t("notifications.subjects.invite_approve_notification") do |format|
+    mail(
+      :to       => register_request.email,
+      :subject  => I18n.t("notifications.subjects.invite_approve_notification")
+    ) do |format|
       format.html
     end
   end
 
   protected
 
-  def email_with_name(user, user_email = true)
-    "#{user.user_appeal} <#{user_email ? user.email : APP_CONFIG['do-not-reply-email']}>"
+  def email_with_name(user, email = APP_CONFIG['do-not-reply-email'])
+    "#{user.user_appeal} <#{email}>"
   end
 
   def subject_for_issue(issue, new_issue = false)
