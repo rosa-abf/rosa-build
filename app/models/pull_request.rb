0f97ddb34a3bb8ffdd89238207e9dff5c46b0798
Alexander Machehin <alexander.machehin@gmail.com>
2013-03-19 00:27:26 +0600

    try to catch no merge errors

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -78,12 +78,16 @@ class PullRequest < ActiveRecord::Base
   def merge!(who)
     return false unless can_merging?
     Dir.chdir(path) do
+      commit = repo.commits(to_ref).first
       system "git config user.name \"#{who.uname}\" && git config user.email \"#{who.email}\""
-      if merge
+      res = merge
+      if commit.id != repo.commits(to_ref).first.id
         system("export GL_ID=user-#{who.id} && git push origin HEAD")
         system("git reset --hard HEAD^") # for diff maybe FIXME
         set_user_and_time who
         merging
+      else # Try to catch no merge errors
+        raise "merge result pull_request #{id}: #{res}"
       end
     end
   end
