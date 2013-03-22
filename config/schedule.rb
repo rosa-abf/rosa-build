1fc21448ceda6e2e71651d1b4669efbedb87cb28
Vokhmin Alexey V <avokhmin@gmail.com>
2013-03-22 22:18:54 +0400

    #31: add task for removing outdated ProductBuildLists

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,19 +7,23 @@
 #  runner "Download.parse_and_remove_nginx_log"
 #end
 
-every 1.day, :at => '4:00 am' do
+every :day, :at => '4:10 am' do
+  rake "product_build_list:clear:outdated", :output => 'log/product_build_list_clear.log'
+end
+
+every :day, :at => '4:00 am' do
   rake "import:sync:all", :output => 'log/sync.log'
 end
 
-every 1.day, :at => '3:50 am' do
+every :day, :at => '3:50 am' do
   rake "buildlist:clear:outdated", :output => 'log/build_list_clear.log'
 end
 
-every 1.day, :at => '3:30 am' do
+every :day, :at => '3:30 am' do
   rake "pull_requests:clear", :output => 'log/pull_requests_clear.log'
 end
 
-every 1.day, :at => '3:00 am' do
+every :day, :at => '3:00 am' do
   rake "activity_feeds:clear", :output => 'log/activity_feeds.log'
 end
 
