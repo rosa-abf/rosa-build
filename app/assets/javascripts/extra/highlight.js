390d80a9a2d09f07628e47b1b20358259e216eb3
Vokhmin Alexey V <avokhmin@gmail.com>
2013-03-28 01:10:34 +0400

    #52: added highlighting for line

---
+++ app/assets/javascripts/extra/highlight.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+  jQuery(window).bind('hashchange', function(e) {
+    var hash = location.hash;
+    if (/^#diff-/.test(hash)) {
+      highlightDiff(hash);
+    } else if (/^#L[0-9]+/.test(hash)) {
+      highlightShow(hash);
+    }
+  });
+
+  // Since the event is only triggered when the hash changes, we need to trigger
+  // the event now, to handle the hash the page may have loaded with.
+  jQuery(window).trigger('hashchange');
+
+});
+
+
+function highlightShow(id) {
+  $('.highlight-line').remove();
+  $(id).append('<div class="highlight-line"></div>');
+}
+
+function highlightDiff(id) {
+  $('.highlight-line').removeClass('highlight-line');
+  $(id).parent().find('td.code').addClass('highlight-line');
+}
