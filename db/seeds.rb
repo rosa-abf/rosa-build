0dfb3bd00fb58764002fd375a99529aaaae15205
Vokhmin Alexey V <avokhmin@gmail.com>
2013-03-29 16:38:06 +0400

    update uname of admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,5 @@ end
   user.confirmed_at, user.role = Time.now.utc, 'system'; user.save
 end
 
-admin = User.new uname: 'admin', email: 'admin@rosalinux.ru', password: 'qwerty'
+admin = User.new uname: 'admin_user', email: 'admin_user@rosalinux.ru', password: 'qwerty'
 admin.confirmed_at, admin.role = Time.now.utc, 'admin'; admin.save
