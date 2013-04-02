2bc580500df7e021b3094acc18065ec90cf2f78f
Vokhmin Alexey V <avokhmin@gmail.com>
2013-04-03 01:23:00 +0400

    #51: Change rights for operations with tags and assignee

--- app/controllers/projects/issues_controller.rb
+++ app/controllers/projects/issues_controller.rb
@@ -41,6 +41,10 @@ class Projects::IssuesController < Projects::BaseController
     @assignee_uname = params[:assignee_uname]
     @issue.user_id = current_user.id
 
+    unless can?(:write, @project)
+      @issue.assignee_id = nil
+      @issue.labelings = []
+    end
     if @issue.save
       @issue.subscribe_creator(current_user.id)
       flash[:notice] = I18n.t("flash.issue.saved")
@@ -56,6 +60,10 @@ class Projects::IssuesController < Projects::BaseController
   end
 
   def update
+    unless can?(:write, @project)
+      [:labelings, :labelings_attributes, :assignee_id].each{ |k| params[:issue].delete k }
+      params.delete :update_labels
+    end
     @issue.labelings.destroy_all if params[:update_labels]
     if params[:issue] && status = params[:issue][:status]
       @issue.set_close(current_user) if status == 'closed'
