588a7d8bab77de30a3615c7f032446b06a86d06a
Vokhmin Alexey V <avokhmin@gmail.com>
2013-04-04 17:36:54 +0400

    #73: remove unnecessary code

--- lib/modules/models/owner.rb
+++ lib/modules/models/owner.rb
@@ -5,9 +5,6 @@ module Modules
       extend ActiveSupport::Concern
 
       included do
-        belongs_to :owner_user, :class_name => 'User', :foreign_key => 'owner_id'
-        belongs_to :owner_group, :class_name => 'Group', :foreign_key => 'owner_id'
-
         validates :owner, :presence => true
         after_create lambda { relations.create :actor_id => owner.id, :actor_type => owner.class.to_s, :role => 'admin' }
       end
