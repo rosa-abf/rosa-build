7677d4301067aaa79bdbb0f0501ebbe0f36a1c44
Alexander Machehin <alexander.machehin@gmail.com>
2013-04-08 18:38:38 +0600

    [#19] copy emoji to public folder

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,5 @@ module Rosa
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    config.assets.paths << Emoji.images_path
   end
 end
