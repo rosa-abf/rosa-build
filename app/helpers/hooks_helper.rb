d805e39716ca108bf1ae6f111eee19baa01ba415
Vokhmin Alexey V <avokhmin@gmail.com>
2013-04-15 14:04:47 +0400

    #34: added before_validation for Hook model

--- app/helpers/hooks_helper.rb
+++ app/helpers/hooks_helper.rb
@@ -1,16 +1,2 @@
 module HooksHelper
-
-  def web_fields
-    {:url => :string}
-  end
-
-  def hipchat_fields
-    {
-      :auth_token => :string,
-      :room => :string,
-      :restrict_to_branch => :string,
-      :notify => :boolean
-    }
-  end
-
 end
--- app/models/hook.rb
+++ app/models/hook.rb
@@ -4,8 +4,19 @@ class Hook < ActiveRecord::Base
     hipchat
   ].freeze
 
+  FIELDS = {
+    :web      => {:url => :string},
+    :hipchat  => {
+      :auth_token => :string,
+      :room => :string,
+      :restrict_to_branch => :string,
+      :notify => :boolean
+    }
+  }
+
   belongs_to :project
 
+  before_validation :cleanup_data
   validates :project_id, :presence => true
   validates :data, :presence => true
   validates :name, :presence => true, :inclusion => {:in => NAMES}
@@ -15,5 +26,15 @@ class Hook < ActiveRecord::Base
   serialize :data,  Hash
 
   scope :for_name, lambda {|name| where(:name => name) if name.present? }
+
+  protected
+
+  def cleanup_data
+    if self.name.present? && fields = FIELDS[self.name.to_sym]
+      new_data = {}
+      fields.each{ |f, t| new_data[f] = self.data[f] }
+      self.data = new_data
+    end
+  end
 
 end
