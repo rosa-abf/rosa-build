f8d0320d239c1d61bd402deaf4325fa65a7b431a
Vokhmin Alexey V <avokhmin@gmail.com>
2013-04-15 17:34:11 +0400

    #34: added new hooks, some refactoring according to Alexander's comments

--- app/models/hook.rb
+++ app/models/hook.rb
@@ -1,24 +1,9 @@
 class Hook < ActiveRecord::Base
-  NAMES = %w[
-    web
-    hipchat
-  ].freeze
-
-  FIELDS = {
-    :web      => {:url => :string},
-    :hipchat  => {
-      :auth_token => :string,
-      :room => :string,
-      :restrict_to_branch => :string,
-      :notify => :boolean
-    }
-  }
-
+  include Modules::Models::WebHooks
   belongs_to :project
 
   before_validation :cleanup_data
-  validates :project_id, :presence => true
-  validates :data, :presence => true
+  validates :project_id, :data, :presence => true
   validates :name, :presence => true, :inclusion => {:in => NAMES}
 
   attr_accessible :data, :name
@@ -30,9 +15,9 @@ class Hook < ActiveRecord::Base
   protected
 
   def cleanup_data
-    if self.name.present? && fields = FIELDS[self.name.to_sym]
+    if self.name.present? && fields = SCHEMA[self.name.to_sym]
       new_data = {}
-      fields.each{ |f, t| new_data[f] = self.data[f] }
+      fields.each{ |type, field| new_data[field] = self.data[field] }
       self.data = new_data
     end
   end
