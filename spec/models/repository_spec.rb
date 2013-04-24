1766c821e03ed0fb9ffa6bb8c134ed9bb9de440b
Vokhmin Alexey V <avokhmin@gmail.com>
2013-04-24 14:41:43 +0400

    #111: added specs for repository and platform models

--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -2,10 +2,36 @@
 require 'spec_helper'
 
 describe Repository do
+  before { stub_symlink_methods }
+
+  context 'ensures that validations and associations exist' do
+    before do
+      # Need for validate_uniqueness_of check
+      FactoryGirl.create(:repository)
+    end
+
+    it { should belong_to(:platform) }
+    it { should have_many(:project_to_repositories).validate(true) }
+    it { should have_many(:projects).through(:project_to_repositories) }
+
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name).case_insensitive.scoped_to(:platform_id) }
+    it { should allow_value('basic_repository-name-1234').for(:name) }
+    it { should_not allow_value('.!').for(:name) }
+    it { should_not allow_value('Main').for(:name) }
+    it { should_not allow_value("!!\nbang_bang\n!!").for(:name) }
+    it { should validate_presence_of(:description) }
+
+    it { should have_readonly_attribute(:name) }
+    it { should have_readonly_attribute(:platform_id) }
+
+    it { should_not allow_mass_assignment_of(:platform) }
+    it { should_not allow_mass_assignment_of(:platform_id) }
+
+  end
 
   context 'when create with same owner that platform' do
-    before (:each) do
-      stub_symlink_methods
+    before do
       @platform = FactoryGirl.create(:platform)
       @params = {:name => 'tst_platform', :description => 'test platform'}
     end
@@ -16,39 +42,31 @@ describe Repository do
     end
   end
 
-  before(:all) do
-    stub_symlink_methods
-    Platform.delete_all
-    User.delete_all
-    Repository.delete_all
-    init_test_root
-    # Need for validate_uniqueness_of check
-    FactoryGirl.create(:repository)
-  end
+  context 'ensures that folder of repository will be removed after destroy' do
+    let(:arch) { FactoryGirl.create(:arch) }
+    let(:types) { ['SRPM', arch.name] }
 
-  it { should belong_to(:platform) }
-  it { should have_many(:project_to_repositories).validate(true) }
-  it { should have_many(:projects).through(:project_to_repositories) }
+    it "repository of main platform" do
+      FactoryGirl.create(:arch)
+      r = FactoryGirl.create(:repository)
+      paths = types.
+        map{ |type| "#{r.platform.path}/repository/#{type}/#{r.name}" }.
+        each{ |path| FileUtils.mkdir_p path }
+      r.destroy
+      paths.each{ |path| Dir.exists?(path).should be_false }
+    end
 
-  it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:name).case_insensitive.scoped_to(:platform_id) }
-  it { should validate_format_of(:name).with('basic_repository-name-1234') }
-  it { should validate_format_of(:name).not_with('.!') }
-  it { should validate_format_of(:name).not_with('Main') }
-  it { should validate_format_of(:name).not_with("!!\nbang_bang\n!!") }
-  it { should validate_presence_of(:description) }
+    it "repository of personal platform" do
+      FactoryGirl.create(:arch)
+      main_platform = FactoryGirl.create(:platform)
+      r = FactoryGirl.create(:personal_repository)
+      paths = types.
+        map{ |type| "#{r.platform.path}/repository/#{main_platform.name}/#{type}/#{r.name}" }.
+        each{ |path| FileUtils.mkdir_p path }
+      r.destroy
+      paths.each{ |path| Dir.exists?(path).should be_false }
+    end
 
-  it { should have_readonly_attribute(:name) }
-  it { should have_readonly_attribute(:platform_id) }
-
-  it { should_not allow_mass_assignment_of(:platform) }
-  it { should_not allow_mass_assignment_of(:platform_id) }
-
-  after(:all) do
-    Platform.delete_all
-    User.delete_all
-    Repository.delete_all
-    clear_test_root
   end
 
 end
