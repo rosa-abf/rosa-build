f000906ec23385f830afacb2cdaf2ab90f1aa398
Alexander Machehin <alexander.machehin@gmail.com>
2013-05-07 16:24:13 +0600

    [#120] show only absolute url for containers

--- app/helpers/build_lists_helper.rb
+++ app/helpers/build_lists_helper.rb
@@ -77,11 +77,10 @@ module BuildListsHelper
     end
   end
 
-  def container_url(full_path = true, build_list = @build_list)
-    p = full_path ? APP_CONFIG['downloads_url'].dup : "http://#{request.host_with_port}/downloads"
-    p << "/#{build_list.save_to_platform.name}/container/#{build_list.id}"
-    p << "/#{build_list.arch.name}/#{build_list.save_to_repository.name}/release" if full_path && build_list.build_for_platform.distrib_type == 'mdv'
-    p.html_safe
+  def container_url(build_list = @build_list)
+    url = "#{APP_CONFIG['downloads_url']}/#{build_list.save_to_platform.name}/container/#{build_list.id}"
+    url << "/#{build_list.arch.name}/#{build_list.save_to_repository.name}/release" if build_list.build_for_platform.distrib_type == 'mdv'
+    url.html_safe
   end
 
   def can_publish_in_future?(bl)
