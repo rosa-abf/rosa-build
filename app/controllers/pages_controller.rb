f8f53b198b8fc83de06d0fb3e3d0ec33cc3a6362
Alexander Machehin <alexander.machehin@gmail.com>
2013-05-15 23:28:39 +0600

    [#98] create home controller;  move activity, common issues/pull_requests & root pages here

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,5 @@
 # -*- encoding : utf-8 -*-
 class PagesController < ApplicationController
-  # before_filter :authenticate_user!, :except => [:show, :main, :forbidden]
-  # load_and_authorize_resource
-
-  def root
-    render 'pages/tour/abf-tour-project-description-1', :layout => 'tour'
-  end
 
   def tour_inside
     @entries = case params[:id]
