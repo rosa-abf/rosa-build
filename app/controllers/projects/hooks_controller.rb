9f73359cca975e15c1bd28fa5c908d4bcd0f9907
Vokhmin Alexey V <avokhmin@gmail.com>
2013-05-16 15:36:29 +0400

    #34: removed unnecessary comments

--- app/controllers/projects/hooks_controller.rb
+++ app/controllers/projects/hooks_controller.rb
@@ -5,32 +5,19 @@ class Projects::HooksController < Projects::BaseController
   load_and_authorize_resource :hook, :through => :project
 
 
-  # GET /uname/project/hooks
-  # GET /uname/project/hooks?name=web
   def index
     authorize! :edit, @project
     @name = params[:name]
     @hooks = @project.hooks.for_name(@name).order('name asc, created_at desc')
-    if @name.present?
-      render :show
-    else
-      render :index
-    end
+    render(:show) if @name.present?
   end
 
-  # GET /uname/project/hooks/1
-  def show
-  end
-
-  # GET /uname/project/hooks/new
   def new
   end
 
-  # GET /uname/project/hooks/1/edit
   def edit
   end
 
-  # POST /uname/project/hooks
   def create
     if @hook.save
       redirect_to project_hooks_path(@project, :name => @hook.name), :notice => t('flash.hook.created')
@@ -41,7 +28,6 @@ class Projects::HooksController < Projects::BaseController
     end
   end
 
-  # PUT /uname/project/hooks/1
   def update
     if @hook.update_attributes(params[:hook])
       redirect_to project_hooks_path(@project, :name => @hook.name), :notice => t('flash.hook.updated')
@@ -52,7 +38,6 @@ class Projects::HooksController < Projects::BaseController
     end
   end
 
-  # DELETE /uname/project/hooks/1
   def destroy
     @hook.destroy
     redirect_to project_hooks_path(@project, :name => @hook.name)
