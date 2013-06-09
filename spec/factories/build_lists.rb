07ff8835985285e8904735bfe3c5eeba17a183f0
Alexander Machehin <alexander.machehin@gmail.com>
2013-06-09 18:46:06 +0600

    [#135] revert build list factory

--- spec/factories/build_lists.rb
+++ spec/factories/build_lists.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
     #association :project
     association :save_to_platform, :factory => :platform_with_repos
     project { |bl|
-      pr = FactoryGirl.create(:project_with_commit, :owner => bl.user)
+      pr = FactoryGirl.create(:project_with_commit)
       bl.save_to_platform.repositories.first.projects << pr
       pr
     }
