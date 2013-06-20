563f983e607e75aa297e5a1a6dd974e0c50ff1df
Vokhmin Alexey V <avokhmin@gmail.com>
2013-06-20 14:37:05 +0400

    #193: hot fix

--- app/models/activity_feed_observer.rb
+++ app/models/activity_feed_observer.rb
@@ -93,7 +93,8 @@ class ActivityFeedObserver < ActiveRecord::Observer
                           :change_type => change_type, :project_owner => record.project.owner.uname}
       else
         if record.message # online update
-          last_commits, commits, all_commits = [[record.newrev, record.message]], [], []
+          last_commits, commits = [[record.newrev, record.message]], []
+          all_commits = last_commits
         else
           commits = record.project.repo.commits_between(record.oldrev, record.newrev)
           all_commits = commits.collect { |commit| [commit.sha, commit.message] }
