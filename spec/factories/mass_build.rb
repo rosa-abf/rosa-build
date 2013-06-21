47ed190b73c687b7a37ef11b378c18f2fb145b53
Vokhmin Alexey V <avokhmin@gmail.com>
2013-06-21 14:39:03 +0400

    #195: updated MassBuild model, added specs

--- spec/factories/mass_build.rb
+++ spec/factories/mass_build.rb
@@ -1,10 +1,10 @@
 # -*- encoding : utf-8 -*-
 FactoryGirl.define do
   factory :mass_build do
-    association :save_to_platform
+    association :save_to_platform, :factory => :platform
     association :user
     projects_list "first"
-    arches { [ Arch.first.id ] }
+    arches { [ Arch.find_or_create_by_name('x86_64').id ] }
     auto_publish true
     stop_build false
   end
