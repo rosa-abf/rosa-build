98504012547cb9875bdbf71eafbe18e1abdf7eca
Vokhmin Alexey V <avokhmin@gmail.com>
2013-06-25 11:01:03 +0400

    #192: updated views

--- app/controllers/platforms/tokens_controller.rb
+++ app/controllers/platforms/tokens_controller.rb
@@ -1,4 +1,4 @@
-class Platforms::TokensController < ApplicationController
+class Platforms::TokensController < Platforms::BaseController
   before_filter :authenticate_user!
 
   # load_and_authorize_resource :platform
