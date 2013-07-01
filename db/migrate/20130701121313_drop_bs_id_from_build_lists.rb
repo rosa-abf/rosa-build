8248ab3614cf78e784674d45fef6d5e21f5c3c6d
Vokhmin Alexey V <avokhmin@gmail.com>
2013-07-01 17:47:15 +0400

    #168: updated migration

--- db/migrate/20130701121313_drop_bs_id_from_build_lists.rb
+++ db/migrate/20130701121313_drop_bs_id_from_build_lists.rb
@@ -1,7 +1,17 @@
 class DropBsIdFromBuildLists < ActiveRecord::Migration
-  def change
-    remove_index :build_lists, :bs_id
+
+  class BuildList < ActiveRecord::Base
+  end
+
+  def up
+    remove_index  :build_lists, :bs_id
     remove_column :build_lists, :bs_id
+  end
+
+  def down
+    add_column  :build_lists, :bs_id, :integer
+    add_index   :build_lists, :bs_id, :unique => true
+    BuildList.update_all('bs_id = id')
   end
 
 end
