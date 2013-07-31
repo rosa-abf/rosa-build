74fdd772d66751780392bb348bf83716f93fe4e4
Vokhmin Alexey V <avokhmin@gmail.com>
2013-07-31 18:01:10 +0400

    #245: added specs for ProjectStatistic model

---
+++ spec/models/project_statistic_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe ProjectStatistic do
+
+  context 'ensures that validations and associations exist' do
+    it { should belong_to(:project) }
+    it { should belong_to(:arch) }
+
+    it { should validate_presence_of(:project_id) }
+    it { should validate_presence_of(:arch_id) }
+    it { should validate_presence_of(:average_build_time) }
+    it { should validate_presence_of(:build_count) }
+
+    it { should_not allow_mass_assignment_of(:project_id) }
+    it { should_not allow_mass_assignment_of(:arch_id) }
+
+    it 'uniqueness of project_id and arch_id' do
+      FactoryGirl.create(:project_statistic)
+      should validate_uniqueness_of(:project_id).scoped_to(:arch_id)
+    end
+
+  end
+
+end
