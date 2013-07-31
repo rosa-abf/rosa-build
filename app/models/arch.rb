b135049195abb3bdc83ace851e1573e0f998a990
Alexander Machehin <alexander.machehin@gmail.com>
2013-08-01 01:17:38 +0600

    [#230] set default arches for personal platforms

--- app/models/arch.rb
+++ app/models/arch.rb
@@ -1,5 +1,7 @@
 # -*- encoding : utf-8 -*-
 class Arch < ActiveRecord::Base
+  DEFAULT = %w[i586 x86_64]
+
   has_many :build_lists, :dependent => :destroy
 
   validates :name, :presence => true, :uniqueness => true
