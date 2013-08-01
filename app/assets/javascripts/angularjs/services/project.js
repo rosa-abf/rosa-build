0db4013bb316eaf99e95b4647cdf48f9fced2844
Vokhmin Alexey V <avokhmin@gmail.com>
2013-08-01 13:26:59 +0400

    Use JSON format for ajax requests

--- app/assets/javascripts/angularjs/services/project.js
+++ app/assets/javascripts/angularjs/services/project.js
@@ -1,31 +1,31 @@
 RosaABF.factory("ApiProject", ['$resource', function($resource) {
 
   var ProjectResource = $resource(
-    '/:owner/:project',
+    '/:owner/:project?format=json',
     {owner: '@project.owner.uname', project: '@project.name'},
     {
       tags: {
-        url:    '/:owner/:project/tags',
+        url:    '/:owner/:project/tags.json',
         method: 'GET',
         isArray : false
       },
       branches: {
-        url:    '/:owner/:project/branches',
+        url:    '/:owner/:project/branches.json',
         method: 'GET',
         isArray : false
       },
       delete_branch: {
-        url:    '/:owner/:project/branches/:ref',
+        url:    '/:owner/:project/branches/:ref.json',
         method: 'DELETE',
         isArray : false
       },
       restore_branch: {
-        url:    '/:owner/:project/branches/:ref', // ?sha=<sha>
+        url:    '/:owner/:project/branches/:ref.json', // ?sha=<sha>
         method: 'PUT',
         isArray : false
       },
       create_branch: {
-        url:    '/:owner/:project/branches', // ?new_ref=<new_ref>&from_ref=<from_ref>
+        url:    '/:owner/:project/branches.json', // ?new_ref=<new_ref>&from_ref=<from_ref>
         method: 'POST',
         isArray : false
       }
