4f8e7ab71e8da6753738151c9d99331e389e5df1
Vokhmin Alexey V <avokhmin@gmail.com>
2013-08-08 16:16:51 +0400

    #258: removed advisories backbone JS

--- app/assets/javascripts/backbone/routers/build_lists_advisories_router.js
+++
@@ -1,9 +0,0 @@
-Rosa.Routers.BuildListsAdvisoriesRouter = Backbone.Router.extend({
-    routes: {},
-
-    initialize: function() {
-        this.advisoriesView = new Rosa.Views.BuildListAdvisoriesView({ model: new Rosa.Models.Advisory() });
-
-        this.advisoriesView.render();
-    }
-});
