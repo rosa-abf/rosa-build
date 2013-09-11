a48ab7f6c1784534fe3812df382118082be8fb1c
Alexander Machehin <alexander.machehin@gmail.com>
2013-09-12 00:14:48 +0600

    [#298] change libs load priority

--- lib/plugins/rpm.rb
+++ lib/plugins/rpm.rb
@@ -6,13 +6,12 @@ module RPM
     extend ::FFI::Library
 
     begin
-      ffi_lib ['rpm', 'librpm.so.2', 'librpm.so.1']
+      ffi_lib ['librpm.so.3', 'librpm.so.2', 'librpm.so.1', 'rpm']
     rescue LoadError => e
       raise(
         "Can't find rpm libs on your system: #{e.message}"
       )
     end
-
 
     attach_function 'rpmvercmp', [:string, :string], :int
 
