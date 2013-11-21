0ef707fd984e5b429e0a3e8632142e2440b53fbb
Vokhmin Alexey V <avokhmin@gmail.com>
2013-11-21 21:59:29 +0400

    Use errbit instead of rollbar

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -2,7 +2,8 @@
 require 'airbrake'
 
 Airbrake.configure do |config|
-  config.api_key  = APP_CONFIG['keys']['airbrake_api_key']
-  config.host     = 'api.rollbar.com'
-  config.secure   = true
+  config.api_key = APP_CONFIG['keys']['airbrake_api_key']
+  config.host    = 'errbit.rosalinux.ru'
+  config.port    = 80
+  config.secure  = config.port == 443
 end
