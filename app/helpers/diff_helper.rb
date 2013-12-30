58025f868145aa8f2b93a5f150f3fb4dda59f101
Vokhmin Alexey V <avokhmin@gmail.com>
2013-12-30 20:19:20 +0400

    small refactoring

--- app/helpers/diff_helper.rb
+++ app/helpers/diff_helper.rb
@@ -37,12 +37,12 @@ module DiffHelper
            end
     prepare(args.merge({:filepath => filepath, :comments => comments, :in_discussion => in_discussion}))
 
-    res = "<table class='diff inline' cellspacing='0' cellpadding='0' ng-non-bindable>"
-    res << "<tbody>"
+    res = '<table class="diff inline" cellspacing="0" cellpadding="0" ng-non-bindable>'
+    res << '<tbody>'
     res << renderer(diff_display.data) #diff_display.render(Git::Diff::InlineCallback.new comments, path)
     res << tr_line_comments(comments) if in_discussion
-    res << "</tbody>"
-    res << "</table>"
+    res << '</tbody>'
+    res << '</table>'
     res.html_safe
   end
 
