bbc710d33f6e0b350cc6937e6255daac940067ad
Alexander Machehin <alexander.machehin@gmail.com>
2014-01-17 02:06:54 +0600

    [#29] try to fix invalid byte sequence in UTF-8

--- lib/ext/core/string.rb
+++ lib/ext/core/string.rb
@@ -1,6 +1,25 @@
 require 'charlock_holmes/string'
 
 class String
+  def default_encoding!
+    default_encoding = Encoding.default_internal || Encoding::UTF_8
+    if ascii_only? # no need to encode if ascii
+      force_encoding(default_encoding)
+    else # should encode
+      options = {:invalid => :replace, :undef => :replace, :replace => ''}
+      if (detected = detect_encoding) && detected[:encoding]
+        force_encoding(detected[:encoding]).encode!(default_encoding, detected[:encoding], options)
+      end
+      scrub('')
+      raise unless valid_encoding? # check result
+    end
+    self
+  rescue
+    replace "--broken encoding: #{detect_encoding[:encoding] || 'unknown'}"
+  # ensure
+  #   return self
+  end
+
   # same as reverse.truncate.reverse
   def rtruncate(length, options = {})
     chars = self.dup.mb_chars
