98d91a761d6bf386577f0818b6c18b706065150b
Vokhmin Alexey V <avokhmin@gmail.com>
2014-02-12 12:27:37 +0400

    rollback changes

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -17,7 +17,7 @@ on_worker_boot do
 
       config = Rails.application.config.database_configuration[Rails.env]
       config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-      config['pool']              = ENV['DB_POOL']      || 21
+      config['pool']              = ENV['DB_POOL']      || 7
 
       ActiveRecord::Base.establish_connection(config)
 
