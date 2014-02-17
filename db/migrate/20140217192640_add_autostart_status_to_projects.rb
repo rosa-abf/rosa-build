0951ef6609e3e3756258cc14801f5f55db2a2412
Vokhmin Alexey V <avokhmin@gmail.com>
2014-02-18 01:07:49 +0400

    #354: cleanup

--- db/migrate/20140217192640_add_autostart_status_to_projects.rb
+++ db/migrate/20140217192640_add_autostart_status_to_projects.rb
@@ -1,6 +1,6 @@
 class AddAutostartStatusToProjects < ActiveRecord::Migration
   def change
     add_column :projects, :autostart_status,  :integer
-    add_column :project_to_repositories, :autostart_options, :hstore#, default: '', null: false
+    add_column :project_to_repositories, :autostart_options, :hstore
   end
 end
