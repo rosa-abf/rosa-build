8dc8f2abb81fecea0da17258ecd3d42bf240079c
Vokhmin Alexey V <avokhmin@gmail.com>
2014-02-27 01:27:54 +0400

    Updated CleanBuildListsQueuesJob

--- app/jobs/clean_build_lists_queues_job.rb
+++ app/jobs/clean_build_lists_queues_job.rb
@@ -6,7 +6,10 @@ class CleanBuildListsQueuesJob
     redis.smembers('queues').each do |key|
       next if key !~ /(user|mass)_build_/
       queue = "queue:#{key}"
-      if redis.llen(queue) == 0
+      last_updated_at = BuildList.where(user_id: key.gsub(/[^\d]/, '')).
+        order('updated_at DESC').limit(1).pluck(:updated_at).first
+      last_updated_at += 5.minutes if last_updated_at
+      if redis.llen(queue) == 0 && (!last_updated_at || last_updated_at < Time.zone.now)
         redis.multi do
           redis.watch queue
           redis.del queue
