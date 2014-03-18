87fc8fca163ade2317d4db3feb49f8430c9150c9
Vokhmin Alexey V <avokhmin@gmail.com>
2014-03-19 00:22:28 +0400

    #345: fixed product_build_list_spec, mass_build_spec

--- spec/models/product_build_list_spec.rb
+++ spec/models/product_build_list_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
 describe ProductBuildList do
-  before do
-    stub_symlink_methods
-  end
+  before { stub_symlink_methods }
 
-  context 'validates' do
+  context 'ensures that validations and associations exist' do
     before do
       arch = double(:arch, id: 123, name: 'x86_64')
       allow(Arch).to receive(:find_by).with(name: 'x86_64').and_return(arch)
     end
 
     it 'is valid given valid attributes' do
-      FactoryGirl.build(:product_build_list).should be_valid
+      arch = FactoryGirl.create(:arch, name: 'x86_64')
+      allow(Arch).to receive(:find_by).with(name: 'x86_64').and_return(arch)
+      FactoryGirl.create(:product_build_list).should be_true
     end
 
     it { should belong_to(:product) }
@@ -20,14 +20,14 @@ describe ProductBuildList do
     it { should ensure_length_of(:main_script).is_at_most(255) }
     it { should ensure_length_of(:params).is_at_most(255) }
 
-    it { should validate_presence_of(:product_id)}
-    it { should validate_presence_of(:status)}
+    it { should validate_presence_of(:product_id) }
+    it { should validate_presence_of(:status) }
 
     ProductBuildList::STATUSES.each do |value|
-      it {should allow_value(value).for(:status)}
+      it { should allow_value(value).for(:status) }
     end
 
-    it {should_not allow_value(555).for(:status)}
+    it { should_not allow_value(555).for(:status) }
 
     it { should have_readonly_attribute(:product_id) }
     #it { should_not allow_mass_assignment_of(:product_id) }
