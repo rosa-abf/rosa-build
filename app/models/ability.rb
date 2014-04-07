81c3f6aaa636e6d7d2d8bde26ed61069e4bb2eff
Vokhmin Alexey V <avokhmin@gmail.com>
2014-04-08 01:37:11 +0400

    #373: rollback changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -18,7 +18,7 @@ class Ability
     can :read, Issue, project: { visibility: 'open' }
     can [:read, :commits, :files], PullRequest, to_project: {visibility: 'open'}
     can [:read, :log, :everything], BuildList, project: {visibility: 'open'}
-    can [:read, :log], ProductBuildList#, product: {platform: {visibility: 'open'}} # double nested hash don't work
+    can [:read, :log], ProductBuildList, product: {platform: {visibility: 'open'}}
     can [:read, :search], Advisory
 
     # Platforms block
