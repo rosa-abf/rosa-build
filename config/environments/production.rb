32bbd69a5827c078807df109a28967b2080b4b13
Alexander Machehin <alexander.machehin@gmail.com>
2014-05-06 17:31:49 +0600

    [#369] change assets precompile array

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rosa::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile +=
     %w(login.css login.js reg_session.css tour.css tour.js gollum/editor/langs/*.js moment/ru.js
-       codemirror_editor.js codemirror_editor.css codemirror.css new_application.css new_application.js)
+       codemirror_editor.js codemirror_editor.css new_application.css new_application.js)
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
