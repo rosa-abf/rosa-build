14d2b7073b5bdc648fb960400196a82d0ca95058
Vokhmin Alexey V <avokhmin@gmail.com>
2014-08-21 00:25:09 +0400

    updated old migrations

--- db/migrate/20111012223006_create_permissions.rb
+++ db/migrate/20111012223006_create_permissions.rb
@@ -1,15 +1,10 @@
 class CreatePermissions < ActiveRecord::Migration
-  def self.up
+  def change
     create_table :permissions do |t|
-      t.integer :id
       t.integer :right_id
       t.integer :role_id
 
       t.timestamps
     end
-  end
-
-  def self.down
-    drop_table :permissions
   end
 end
--- db/migrate/20111012223306_create_roles.rb
+++ db/migrate/20111012223306_create_roles.rb
@@ -1,14 +1,10 @@
 class CreateRoles < ActiveRecord::Migration
-  def self.up
+  def change
     create_table :roles do |t|
       t.integer :id
       t.string :name
 
       t.timestamps
     end
-  end
-
-  def self.down
-    drop_table :roles
   end
 end
