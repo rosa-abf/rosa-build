6ce333c5a9cfa0ac9f03fa66958f65df1ca5a99f
Vokhmin Alexey V <avokhmin@gmail.com>
2014-10-14 23:44:26 +0400

    upadted specs

--- spec/models/build_list_spec.rb
+++ spec/models/build_list_spec.rb
@@ -236,7 +236,7 @@ describe BuildList do
     end
 
     it 'ensures that return true if version of packages are same and platform RHEL is released' do
-      build_list.save_to_platform.update_attributes(released: true)
+      build_list.save_to_platform.update_attributes(released: true, distrib_type: 'rhel')
       build_list.has_new_packages?.should be_true
     end
 
