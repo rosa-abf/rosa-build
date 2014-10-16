c5ea3d7c6665b4e85358e61ee428fd50e6ae204b
Alexander Machehin <alexander.machehin@gmail.com>
2014-10-16 16:43:04 +0600

    [#369] remove moment.js deprecate warnings

--- app/assets/javascripts/angular-new/controllers/rosa_abf_controller.js
+++ app/assets/javascripts/angular-new/controllers/rosa_abf_controller.js
@@ -2,7 +2,7 @@ RosaABF.controller('RosaABFController', ['$scope', 'LocalesHelper', 'SoundNotifi
                    function($scope, LocalesHelper, SoundNotificationsHelper) {
   $scope.init = function(locale, sound_notifications) {
   	LocalesHelper.setLocale(locale);
-    moment.lang(locale);
+    moment.locale(locale);
     SoundNotificationsHelper.enabled(sound_notifications);
   }
 }]);
