35c6dfd21b64ea20a4611a3a790a7299aa7cba74
Vokhmin Alexey V <avokhmin@gmail.com>
2014-10-27 23:56:52 +0300

    #369: updated UI for maintainers

--- app/controllers/platforms/maintainers_controller.rb
+++ app/controllers/platforms/maintainers_controller.rb
@@ -1,12 +1,15 @@
 class Platforms::MaintainersController < ApplicationController
+  layout 'bootstrap'
+
   before_filter :authenticate_user!
   skip_before_filter :authenticate_user!, only: [:index] if APP_CONFIG['anonymous_access']
   load_and_authorize_resource :platform
 
   def index
-    @maintainers = BuildList::Package.includes(:project)
-                                     .actual.by_platform(@platform)
-                                     .like_name(params[:q])
-                                     .paginate(page: params[:page])
+    @maintainer   = BuildList::Package.new(params[:build_list_package])
+    @maintainers  = BuildList::Package.includes(:project).
+      actual.by_platform(@platform).
+      like_name(@maintainer.name).
+      paginate(page: params[:page])
   end
 end
