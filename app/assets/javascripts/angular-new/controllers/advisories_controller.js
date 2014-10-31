b10387c91e5168e90eb46355e3f9339644e38e20
Alexander Machehin <alexander.machehin@gmail.com>
2014-10-31 15:25:52 +0500

    [#369] fix an advisory pagination

--- app/assets/javascripts/angular-new/controllers/advisories_controller.js
+++ app/assets/javascripts/angular-new/controllers/advisories_controller.js
@@ -4,16 +4,14 @@ RosaABF.controller('AdvisoryCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.advisories  = null;
   $scope.q           = null;
 
-  $scope.init = function(total_items, page, q) {
-    $scope.total_items = total_items;
-    $scope.page        = page;
+  $scope.init = function(q) {
     $scope.q           = q;
   };
 
   $scope.getAdvisories = function() {
     $http.get(Routes.advisories_path({format: 'json', page: $scope.page, q: $scope.q})).then(function(res) {
       $scope.page        = res.data.page;
-      $scope.total_items = parseInt(res.data.advisories_count, 10);
+      $scope.total_items = res.data.advisories_count;
       $scope.advisories  = res.data.advisories;
     });
   };
