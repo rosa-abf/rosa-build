ec6aebfd5b99ad47e01d3771c8f53b87204c3142
Vokhmin Alexey V <avokhmin@gmail.com>
2014-11-18 00:17:03 +0300

    #369: updated UI for search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,13 @@
 class SearchController < ApplicationController
+  include PaginateHelper
+
   before_filter :authenticate_user! unless APP_CONFIG['anonymous_access']
   # load_and_authorize_resource
 
   def index
-    @type = params[:type] || 'all'
-    @query = params[:query]
-    Search.by_term_and_type(
-      @query,
-      @type,
-      current_ability,
-      {page: params[:page]}
-    ).each do |k, v|
-      var = :"@#{k}"
-      instance_variable_set var, v unless instance_variable_defined?(var)
-    end
+    @type       = Search::TYPES.find{ |t| t == params[:type] } || Search::TYPES.first
+    @query      = params[:query]
+    @search     = Search.new(@query, current_ability, paginate_params)
+    @collection = @search.send(@type)
   end
 end
