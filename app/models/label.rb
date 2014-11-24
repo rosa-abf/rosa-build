8cf44dfa0734cb5928f55aa88c29f1b3601d68a3
Alexander Machehin <alexander.machehin@gmail.com>
2014-11-25 01:30:11 +0500

    add Label validations; add attr_accessible

--- app/models/label.rb
+++ app/models/label.rb
@@ -4,7 +4,10 @@ class Label < ActiveRecord::Base
   belongs_to :project
 
   validates :name, uniqueness: { scope: :project_id }
-  validates :name, :color, presence: true
+  validates :name, length: { in: 1..20 }
+
+  validates :color, presence: true
   validates :color, format: { with: /\A([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})\z/, message: I18n.t('layout.issues.invalid_labels') }
 
+  attr_accessible :name, :color
 end
