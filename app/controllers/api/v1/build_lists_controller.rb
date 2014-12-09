72992a5dcdec135c7c14531659af20318594e49e
Alexander Machehin <alexander.machehin@gmail.com>
2014-12-09 18:32:55 +0500

    fix error in api - get a list build lists for a project

--- app/controllers/api/v1/build_lists_controller.rb
+++ app/controllers/api/v1/build_lists_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::BuildListsController < Api::V1::BaseController
   before_filter :authenticate_user!
   skip_before_filter :authenticate_user!, only: [:show, :index] if APP_CONFIG['anonymous_access']
 
-  load_resource :project, only: :index, parent: false
   load_and_authorize_resource :build_list, only: [:show, :create, :cancel, :publish, :reject_publish, :create_container, :publish_into_testing, :rerun_tests]
 
   def show
@@ -10,6 +9,7 @@ class Api::V1::BuildListsController < Api::V1::BaseController
   end
 
   def index
+    @project = Project.find(params[:project_id]) if params[:project_id].present?
     authorize!(:show, @project) if @project
     filter = BuildList::Filter.new(@project, current_user, current_ability, params[:filter] || {})
     @build_lists = filter.find.includes(:save_to_platform, :project, :user, :arch)
