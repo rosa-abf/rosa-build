56fb9016abaf308c6139e05045068c31c0467438
Alexander Machehin <alexander.machehin@gmail.com>
2015-02-02 23:20:45 +0500

    remove redirect when render error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -81,7 +81,7 @@ class ApplicationController < ActionController::Base
   def render_error(status)
     respond_to do |format|
       format.json { render json: {status: status, message: t("flash.#{status}_message")}.to_json, status: status }
-      format.all { redirect_to "/#{status}.html", alert: t("flash.#{status}_message") }
+      format.all  { render "/#{status}.html", alert: t("flash.#{status}_message") }
     end
   end
 
