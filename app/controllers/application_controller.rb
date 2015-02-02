f1daf3d1464ccb13d91ef2d44fd6bfef20b34c02
Alexander Machehin <alexander.machehin@gmail.com>
2015-02-03 01:27:53 +0500

    add status to the error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -81,7 +81,8 @@ class ApplicationController < ActionController::Base
   def render_error(status)
     respond_to do |format|
       format.json { render json: {status: status, message: t("flash.#{status}_message")}.to_json, status: status }
-      format.all  { render file: "public/#{status}.html", alert: t("flash.#{status}_message"), layout: false }
+      format.all  { render file: "public/#{status}.html", status: status,
+                           alert: t("flash.#{status}_message"), layout: false }
     end
   end
 
