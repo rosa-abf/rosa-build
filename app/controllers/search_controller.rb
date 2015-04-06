86bfd9b8c77b397bcb5d88d3f1c2f8fbbdc81106
Vokhmin Alexey V <avokhmin@gmail.com>
2015-04-07 01:56:06 +0300

    #465: cleanup

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,12 +2,11 @@ class SearchController < ApplicationController
   include PaginateHelper
 
   before_action :authenticate_user! unless APP_CONFIG['anonymous_access']
-  # load_and_authorize_resource
 
   def index
     @type       = Search::TYPES.find{ |t| t == params[:type] } || Search::TYPES.first
     @query      = params[:query]
-    @search     = Search.new(@query, current_ability, paginate_params)
+    @search     = Search.new(@query, current_user, paginate_params)
     @collection = @search.send(@type)
   end
 end
