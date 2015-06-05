7b7980ac1515ee2a93dd4c76f14037a73ef9ec48
Vokhmin Alexey V <avokhmin@gmail.com>
2015-06-05 20:55:43 +0300

    Fixed: ActiveModel::ForbiddenAttributesError

--- app/controllers/platforms/maintainers_controller.rb
+++ app/controllers/platforms/maintainers_controller.rb
@@ -3,10 +3,14 @@ class Platforms::MaintainersController < Platforms::BaseController
   skip_before_action :authenticate_user!, only: [:index] if APP_CONFIG['anonymous_access']
 
   def index
-    @maintainer   = BuildList::Package.new(params[:build_list_package])
+    @maintainer   = BuildList::Package.new(build_list_package_params)
     @maintainers  = BuildList::Package.includes(:project).
       actual.by_platform(@platform).
       like_name(@maintainer.name).
       paginate(page: params[:page])
+  end
+
+  def build_list_package_params
+    permit_params :build_list_package, :name
   end
 end
