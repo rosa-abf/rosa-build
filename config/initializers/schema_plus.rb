4130672069370bdaba096a040ce753e5f37625b6
Vokhmin Alexey V <avokhmin@gmail.com>
2015-06-05 21:56:39 +0300

    Fixed: DEPRECATION WARNING

--- config/initializers/schema_plus.rb
+++ config/initializers/schema_plus.rb
@@ -1,3 +1,4 @@
-SchemaPlus.setup do |config|
-  config.foreign_keys.auto_create = false
+SchemaPlus::ForeignKeys.setup do |config|
+  config.auto_create = true # default for schema_auto_foreign_keys
+  # config.auto_index = true # default for schema_auto_foreign_keys
 end
