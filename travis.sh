eefcfdd1da784037707405c8f1e7ad5bf282e483
Vokhmin Alexey V <avokhmin@gmail.com>
2015-06-09 03:26:35 +0300

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -8,14 +8,17 @@ then
 elif [ $SPEC_GROUP = 'platform_controllers' ]
 then
   $rspec spec/controllers/platforms/
+elif [ $SPEC_GROUP = 'api-slow' ]
+then
+  $rspec spec/controllers/api/v1/ --pattern build_lists*
 elif [ $SPEC_GROUP = 'api' ]
 then
-  $rspec spec/controllers/api/
+  $rspec spec/controllers/api/v1/ --exclude-pattern build_lists*
 elif [ $SPEC_GROUP = 'models' ]
 then
   $rspec spec/models/ spec/jobs/
 elif [ $SPEC_GROUP = 'others' ]
 then
-  # $rspec spec/helpers/ spec/integration/ spec/lib/ spec/mailers/ spec/mailers/ spec/routing/ 
-  $rspec spec/integration/ spec/services/ spec/mailers/ spec/mailers/ spec/routing/ 
+  # $rspec spec/helpers/ spec/integration/ spec/lib/ spec/mailers/ spec/mailers/ spec/routing/
+  $rspec spec/integration/ spec/services/ spec/mailers/ spec/mailers/ spec/routing/
 fi
