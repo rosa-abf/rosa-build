b57fb4fc336d797777d173cf1434228a66e8b376
Vokhmin Alexey V <avokhmin@gmail.com>
2015-06-10 22:58:46 +0300

    Disable auto create of indexes

--- config/initializers/schema_plus.rb
+++ config/initializers/schema_plus.rb
@@ -1,4 +1,4 @@
 SchemaPlus::ForeignKeys.setup do |config|
-  config.auto_create = true # default for schema_auto_foreign_keys
+  config.auto_create = false
   # config.auto_index = true # default for schema_auto_foreign_keys
 end
