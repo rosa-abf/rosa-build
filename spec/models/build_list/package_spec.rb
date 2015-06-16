a1a70b562bbbc20afbc2a2e020baa4623e462137
Vokhmin Alexey V <avokhmin@gmail.com>
2015-06-17 00:00:57 +0300

    Fixed: ActiveRecord::SerializationTypeMismatch

--- spec/models/build_list/package_spec.rb
+++ spec/models/build_list/package_spec.rb
@@ -4,17 +4,18 @@ describe BuildList::Package do
   before { stub_symlink_methods }
 
   it 'is valid' do
-    FactoryGirl.create(:build_list_package).should be_persisted
+    expect(FactoryGirl.create(:build_list_package)).to be_persisted
   end
 
-  context '#set_epoch' do
+
+  describe '#set_epoch' do
     let(:package) { FactoryGirl.build(:build_list_package) }
 
     ['', '(none)'].each do |epoch|
       it "ensures that epoch is set to nil when epoch is '#{epoch}'" do
         package.epoch = epoch
         package.save
-        package.epoch.should be_nil
+        expect(package.epoch).to be_nil
       end
 
     end
@@ -22,9 +23,16 @@ describe BuildList::Package do
     it "ensures that valid epoch has been setted" do
       package.epoch = '55'
       package.save
-      package.epoch.should == 55
+      expect(package.epoch).to eq 55
     end
 
   end
 
+  describe '#dependent_packages=' do
+    it 'sets a packages' do
+      package = FactoryGirl.build(:build_list_package, dependent_packages: 'x y z')
+      expect(package).to be_valid
+      expect(package.dependent_packages).to eq %w(x y z)
+    end
+  end
 end
