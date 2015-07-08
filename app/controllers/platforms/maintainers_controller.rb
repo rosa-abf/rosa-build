6563b3138e7a458e4fb0e0185c2eb17fb9a60d02
Vokhmin Alexey V <avokhmin@gmail.com>
2015-07-09 00:54:57 +0300

    #483: Update /maintainers page

--- app/controllers/platforms/maintainers_controller.rb
+++ app/controllers/platforms/maintainers_controller.rb
@@ -3,11 +3,11 @@ class Platforms::MaintainersController < Platforms::BaseController
   skip_before_action :authenticate_user!, only: [:index] if APP_CONFIG['anonymous_access']
 
   def index
-    @maintainer   = BuildList::Package.new(build_list_package_params)
-    @maintainers  = BuildList::Package.includes(:project).
-      actual.by_platform(@platform).
-      like_name(@maintainer.name).
-      paginate(page: params[:page])
+    @maintainer = BuildList::Package.new(build_list_package_params)
+    @packages = @platform.packages.actual.like_name(@maintainer.name)
+    @projects = @platform.projects.joins(:packages).merge( @packages ).
+      includes(:maintainer).group('projects.id').reorder(:name).paginate(page: params[:page])
+    @packages  = @packages.where(project_id: @projects.map(&:id)).group_by(&:project_id)
   end
 
   def build_list_package_params
