785498fc52f909c3d9edef46569272fca285c097
HisShadow <shadowpilot34@gmail.com>
2019-02-24 16:56:07 +0300

    Fix locale template

--- app/models/concerns/url_helper.rb
+++ app/models/concerns/url_helper.rb
@@ -1,6 +1,6 @@
 module UrlHelper
   def default_url_options
-    host ||= EventLog.current_controller.request.host_with_port rescue APP_CONFIG['abf_host']
+    host ||= EventLog.current_controller.request.host_with_port rescue ::Rosa::Application.config.action_mailer.default_url_options[:host]
     protocol ||= APP_CONFIG['mailer_https_url'] ? 'https' : 'http' rescue 'http'
     { host: host, protocol: protocol }
   end
