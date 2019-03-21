0d887d307c9d817a50a64ebc47c1c2caceed42d7
Wedge <shadowpilot34@gmail.com>
2019-03-21 19:09:52 +0300

    Fixes to the builder page

--- app/admin/builders.rb
+++ app/admin/builders.rb
@@ -4,12 +4,19 @@ ActiveAdmin.register_page 'Builders' do
       table_for RpmBuildNode.all.to_a.select { |b| b.get_ttl > 0 }, class: "index_table index" do
         column :id
         column :user_id do |b|
-          User.find(b.user_id) rescue 'User not found.'
+          u = User.find(b.user_id) rescue nil
+          if u
+            link_to u.uname, admin_user_path(u.id)
+          else
+            'None'
+          end
         end
         column :system
-        column :hostname
+        column :host
         column :busy_workers
-        column :query_string
+        column :query_string do |b|
+          b.query_string.present? ? b.query_string : '-'
+        end
         column 'Last build ID' do |b|
           if b.last_build_id.present?
             link_to b.last_build_id, build_list_path(b.last_build_id)
