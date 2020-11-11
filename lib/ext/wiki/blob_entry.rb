27ceb25de680e90234423b402fb747cac5ffad2b
Wedge <shadowpilot34@gmail.com>
2020-11-11 20:35:49 +0300

    Monkey patch gollum blob entry to understand utf-8 filenames, hopefuly it'll work

---
+++ lib/ext/wiki/blob_entry.rb
@@ -0,0 +1,12 @@
+puts 11111111
+class Gollum::BlobEntry
+  def name
+    @name ||= begin
+      fname = ::File.basename(@path)
+      fname = fname.gsub(/\\\d+/).each { |q| q[1..-1].to_i(8).chr }.force_encoding('utf-8')
+      fname.gsub!(/^"/, '')
+      fname.gsub!(/"$/, '')
+      fname
+    end
+  end
+end
