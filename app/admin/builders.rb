91cb898e8392f736e566ca76572f996ae903a5c4
Wedge <shadowpilot34@gmail.com>
2021-08-28 12:41:31 +0300

    Change builder table, group by query string

--- app/admin/builders.rb
+++ app/admin/builders.rb
@@ -1,23 +1,35 @@
 ActiveAdmin.register_page 'Builders' do
   content do
     div class: 'index_as_table' do
-      table_for RpmBuildNode.all.to_a, class: "index_table index" do
-        column :id
-        column :user_id do |b|
-          u = User.find_by_id(b.user_id)
-          u.present? ? link_to(u.uname, admin_user_path(u.id)) : 'Unknown'
+      nodes = RpmBuildNode.all.to_a
+      groups = nodes.group_by { |node| node.query_string.presence || '-' }
+      groups.keys.sort.each do |key|
+        description = if key != '-'
+          parsed_key = Rack::Utils.parse_nested_query(key)
+          parsed_key.delete('native_arches')
+          tmp = parsed_key.keys.map do |prop|
+            "#{prop.capitalize.gsub('_', ' ')}: #{parsed_key[prop].gsub(',', ' ')}"
+          end + ["Total builders: #{groups[key].count}"]
+          tmp.join(', ')
+        else
+          'Everything'
         end
-        column :system
-        column :host
-        column :busy_workers
-        column :query_string do |b|
-          b.query_string.presence || '-'
-        end
-        column 'Last build ID' do |b|
-          if b.last_build_id.present?
-            link_to b.last_build_id, build_list_path(b.last_build_id)
-          else
-            'None'
+        panel description do
+          table_for groups[key], class: "index_table index" do
+            column :host
+            column :user_id do |b|
+              u = User.find_by_id(b.user_id)
+              u.present? ? link_to(u.uname, admin_user_path(u.id)) : 'Unknown'
+            end
+            column :system
+            column :busy_workers
+            column 'Last build ID' do |b|
+              if b.last_build_id.present?
+                link_to b.last_build_id, build_list_path(b.last_build_id)
+              else
+                'None'
+              end
+            end
           end
         end
       end
